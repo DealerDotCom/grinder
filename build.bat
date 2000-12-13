558bffbbcf04f8bee63c3ebe26f35265722c0c9b
Philip Aston <philipa@mail.com>
2000-12-13 23:40:33 +0000

    Fix.

--- build.bat
+++ build.bat
@@ -2,28 +2,11 @@
 
 if "%JAVA_HOME%" == "" goto error
 
-if not "%LIB_HOME%" == "" goto skip
+set ANT_HOME=.\etc\ant
 
-set LIB_HOME=.\lib
+set LOCALCLASSPATH=%ANT_HOME%\ant_1_1.jar;%ANT_HOME%\xerces_1_2.jar;%JAVA_HOME%\lib\tools.jar
 
-:skip
-
-echo.
-echo Grinder Build System
-echo --------------------
-
-set ANT_HOME=.\ant
-
-set LOCALCLASSPATH=%ANT_HOME%\ant_1_1.jar;%JAVA_HOME%\lib\tools.jar
-for %%i in (%LIB_HOME%\*.jar) do call lcp.bat %%i
-
-echo.
-echo Building with classpath %LOCALCLASSPATH%
-
-echo.
-echo Starting Ant...
-
-%JAVA_HOME%\bin\java.exe -Dant.home="%ANT_HOME%" -classpath "%LOCALCLASSPATH%" org.apache.tools.ant.Main %1 %2 %3 %4 %5
+%JAVA_HOME%\bin\java.exe -Dant.home="%ANT_HOME%" -classpath "%LOCALCLASSPATH%" org.apache.tools.ant.Main -buildfile etc/build.xml %1 %2 %3 %4 %5
 
 goto end
 
@@ -38,4 +21,3 @@ echo "location of the Java Virtual Machine you want to use."
 
 set LOCALCLASSPATH=
 set ANT_HOME=
-set LIB_HOME=
