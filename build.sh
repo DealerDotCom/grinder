8fd7dd33bfc26c6d93b801eca071059fff595db2
Philip Aston <philipa@mail.com>
2000-12-18 11:36:48 +0000

    Simplify cygwin handling.

--- build.sh
+++ build.sh
@@ -12,11 +12,9 @@ ANT_HOME=./etc/ant
 
 if [ -n "${CYGWIN}" ]
 then
-    ANT_HOME=`cygpath -w ${ANT_HOME}`
-
-    LOCALCLASSPATH="${ANT_HOME}\ant_1_1.jar;${ANT_HOME}\xerces_1_2.jar;${JAVA_HOME}\lib\tools.jar;${CLASSPATH}"
-else
-    LOCALCLASSPATH="${ANT_HOME}/ant_1_1.jar:${ANT_HOME}/xerces_1_2.jar:${JAVA_HOME}/lib/tools.jar:${CLASSPATH}"
+    ANT_HOME=`cygpath -w "${ANT_HOME}"`
 fi
+
+LOCALCLASSPATH="${ANT_HOME}/ant_1_1.jar;${ANT_HOME}/xerces_1_2.jar;${JAVA_HOME}/lib/tools.jar;$CLASSPATH"
 
 ${JAVA_HOME}/bin/java -Dant.home=$ANT_HOME -classpath $LOCALCLASSPATH org.apache.tools.ant.Main -buildfile etc/build.xml $*
