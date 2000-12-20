0b330b53c9fff0a10a740092033e09e1b54313fb
Philip Aston <philipa@mail.com>
2000-12-20 11:56:44 +0000

    Fix.

--- etc/buildenv.sh
+++ etc/buildenv.sh
@@ -1,2 +1,4 @@
-export ANT_HOME=e:\ant\ant1.2
+CYGWIN_ANT_HOME=/opt/ant/ant1.2
+export ANT_HOME=$(cygpath -w ${CYGWIN_ANT_HOME})
 export JAVA_HOME=e:\jdk1.3
+export PATH=$PATH:${CYGWIN_ANT_HOME}/bin
