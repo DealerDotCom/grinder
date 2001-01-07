9d69eb88071f0a539d0d68193728f6765e98d673
Philip Aston <philipa@mail.com>
2001-01-07 13:05:01 +0000

    Change for new directory structure.

--- etc/buildenv.sh
+++ etc/buildenv.sh
@@ -5,6 +5,6 @@ export PATH=$PATH:${CYGWIN_ANT_HOME}/bin
 
 unset CLASSPATH
 
-. /src/scripts/wls/wls5.1-client.bash
+. /work/src/scripts/wls/wls5.1-client.bash
 
 addToWindowsClasspath CLASSPATH ${WLS_HOME}/examples/ejb/basic/statelessSession/build/ejb_basic_statelessSession.jar
