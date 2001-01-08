c52e745b93aac20e1e2336ffe6aab2aa1ad7064a
Philip Aston <philipa@mail.com>
2001-01-08 08:10:01 +0000

    Fix for new world order.

--- src/prj.el
+++ src/prj.el
@@ -1,9 +1,7 @@
 
 (jde-set-project-name "Grinder")
 (jde-set-variables 
- '(jde-global-classpath (quote ("/src/grinder/src;//e/wls5.1/sp6/weblogic510sp6.jar;//e/wls5.1/lib/weblogicaux.jar;//e/wls5.1/classes;/src/java;//e/wls5.1/license;//e/junit/junit-1.0/test.jar;e://wls5.1/;//e/jdk1.3/jre/lib/ext/jnet.jar;//e/jdk1.3/jre/lib/ext/jcert.jar;//e/jdk1.3/jre/lib/ext/jsse.jar;//e/jdk1.3/jre/lib/rt.jar")))
+ '(jde-global-classpath (quote ("/work/src/grinder/src;/opt/wls5.1/sp6/weblogic510sp6.jar;/opt/wls5.1/lib/weblogicaux.jar;/opt/wls5.1/classes;/opt/wls5.1/license;/opt/junit/junit-1.0/test.jar;/opt/jdk1.3/jre/lib/ext/jnet.jar;/opt/jdk1.3/jre/lib/ext/jcert.jar;/opt/jdk1.3/jre/lib/ext/jsse.jar;/opt/jdk1.3/jre/lib/rt.jar")))
 
- '(jde-run-option-classpath  (quote ("/src/grinder/build/classes;//e/wls5.1/sp6/weblogic510sp6.jar;//e/wls5.1/lib/weblogicaux.jar;//e/wls5.1/classes;/src/java;//e/wls5.1/license;//e/junit/junit-1.0/test.jar;e://wls5.1/")))
-
- '(jde-compile-option-directory "/src/grinder/build/classes")
+ '(jde-compile-option-directory "/work/src/grinder/build/classes")
 )
