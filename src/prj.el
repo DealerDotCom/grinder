a05848c6c748c3e1d0ca3eaceb947cea5a66180a
Philip Aston <philipa@mail.com>
2001-01-11 08:48:51 +0000

    New world order.

--- src/prj.el
+++ src/prj.el
@@ -1,7 +1,7 @@
 
 (jde-set-project-name "Grinder")
 (jde-set-variables 
- '(jde-global-classpath (quote ("/work/src/grinder/src;/opt/wls5.1/sp6/weblogic510sp6.jar;/opt/wls5.1/lib/weblogicaux.jar;/opt/wls5.1/classes;/opt/wls5.1/license;/opt/junit/junit-1.0/test.jar;/opt/jdk1.3/jre/lib/ext/jnet.jar;/opt/jdk1.3/jre/lib/ext/jcert.jar;/opt/jdk1.3/jre/lib/ext/jsse.jar;/opt/jdk1.3/jre/lib/rt.jar")))
+ '(jde-global-classpath (quote ("/work/src/grinder/src;/opt/wls5.1/sp6/weblogic510sp6.jar;/opt/wls5.1/lib/weblogicaux.jar;/opt/wls5.1/classes;/opt/wls5.1/license;/opt/junit/junit-1.0/test.jar;/opt/jdk1.3/jre/lib/ext/jnet.jar;/opt/jdk1.3/jre/lib/ext/jcert.jar;/opt/jdk1.3/jre/lib/ext/jsse.jar;/opt/jdk1.3/jre/lib/rt.jar;/opt/jakarta-regexp/jakarta-regexp-1.2/jakarta-regexp-1.2.jar")))
 
  '(jde-compile-option-directory "/work/src/grinder/build/classes")
 )
