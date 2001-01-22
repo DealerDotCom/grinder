748ac1021f53e4e4c7e0f077c3bb48447fae83b0
Philip Aston <philipa@mail.com>
2001-01-22 22:16:09 +0000

    Add the copyright info.

--- tests-src/net/grinder/engine/process/TestTest.java
+++ tests-src/net/grinder/engine/process/TestTest.java
@@ -1,3 +1,21 @@
+// The Grinder
+// Copyright (C) 2001  Paco Gomez
+// Copyright (C) 2001  Philip Aston
+
+// This program is free software; you can redistribute it and/or
+// modify it under the terms of the GNU General Public License
+// as published by the Free Software Foundation; either version 2
+// of the License, or (at your option) any later version.
+
+// This program is distributed in the hope that it will be useful,
+// but WITHOUT ANY WARRANTY; without even the implied warranty of
+// MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+// GNU General Public License for more details.
+
+// You should have received a copy of the GNU General Public License
+// along with this program; if not, write to the Free Software
+// Foundation, Inc., 59 Temple Place - Suite 330, Boston, MA  02111-1307, USA.
+
 package net.grinder.engine.process;
 
 import junit.framework.TestCase;
@@ -11,8 +29,6 @@ import java.util.Properties;
 import net.grinder.util.GrinderProperties;
 
 
-/**
- */
 public class TestTest extends TestCase
 {
     public static void main(String[] args)
