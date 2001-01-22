cfb8821a67a817fef7f1cb5cd7a3a76b1b07b1f7
Philip Aston <philipa@mail.com>
2001-01-22 22:28:28 +0000

    Collate the tests.

---
+++ tests-src/net/grinder/engine/process/AllTests.java
@@ -0,0 +1,41 @@
+// The Grinder
+// Copyright (C) 2001  Paco Gomez
+// Copyright (C) 2001  Philip Aston
+
+// This program is free software; you can redistribute it and/or
+// modify it under the terms of the GNU General Public License
+// as published by the Free Software Foundation; either version 2
+// of the License, or (at your option) any later version.
+
+// This program is distributed in the hope that it will be useful,
+// but WITHOUT ANY WARRANTY; without even the implied warranty of
+// MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+// GNU General Public License for more details.
+
+// You should have received a copy of the GNU General Public License
+// along with this program; if not, write to the Free Software
+// Foundation, Inc., 59 Temple Place - Suite 330, Boston, MA  02111-1307, USA.
+
+package net.grinder.engine.process;
+
+import junit.framework.Test;
+import junit.framework.TestSuite;
+import junit.swingui.TestRunner;
+//import junit.textui.TestRunner;
+
+
+
+public class AllTests
+{
+    public static void main(String[] args)
+    {
+	TestRunner.run(AllTests.class);
+    }
+
+    public static Test suite()
+    {
+	final TestSuite suite = new TestSuite();
+	suite.addTest(new TestSuite(TestTest.class));
+	return suite;
+    }
+}
