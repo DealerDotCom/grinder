b61bcd30932d5957b9229ddc8530b5d50c713098
Philip Aston <philipa@mail.com>
2001-01-22 22:28:39 +0000

    Fix.

--- tests-src/prj.el
+++ tests-src/prj.el
@@ -1,7 +1,7 @@
 
 (jde-set-project-name "Grinder tests")
 (jde-set-variables 
- '(jde-global-classpath (quote ("/work/src/grinder/build/classes;/opt/junit/junit3.4/junit.jar;/opt/jdk1.3/jre/lib/rt.jar")))
+ '(jde-global-classpath (quote ("/work/src/grinder/tests-src;/work/src/grinder/build/classes;/opt/junit/junit3.4/junit.jar;/opt/jdk1.3/jre/lib/rt.jar")))
 
  '(jde-run-option-classpath (quote ("d:/src/grinder/build/tests-classes;d:/src/grinder/build/classes;e:/junit/junit3.4/junit.jar")))
 
