fbae47a816eaf21dabdae7008e6e92135ab0df96
Philip Aston <philipa@mail.com>
2001-01-22 22:13:31 +0000

    Fixettes.

--- src/prj.el
+++ src/prj.el
@@ -1,7 +1,7 @@
 
 (jde-set-project-name "Grinder")
 (jde-set-variables 
- '(jde-global-classpath (quote ("/work/src/grinder/src;/opt/wls5.1/sp6/weblogic510sp6.jar;/opt/wls5.1/lib/weblogicaux.jar;/opt/wls5.1/classes;/opt/wls5.1/license;/opt/junit/junit-1.0/test.jar;/opt/jdk1.3/jre/lib/ext/jnet.jar;/opt/jdk1.3/jre/lib/ext/jcert.jar;/opt/jdk1.3/jre/lib/ext/jsse.jar;/opt/jdk1.3/jre/lib/rt.jar;/opt/jakarta-regexp/jakarta-regexp-1.2/jakarta-regexp-1.2.jar:/opt/junit/junit3.4/junit.jar;/opt/wls5.1/examples/ejb/basic/statelessSession/build/ejb_basic_statelessSession.jar")))
+ '(jde-global-classpath (quote ("/work/src/grinder/src;/opt/wls5.1/sp6/weblogic510sp6.jar;/opt/wls5.1/lib/weblogicaux.jar;/opt/wls5.1/classes;/opt/wls5.1/license;/opt/jdk1.3/jre/lib/ext/jnet.jar;/opt/jdk1.3/jre/lib/ext/jcert.jar;/opt/jdk1.3/jre/lib/ext/jsse.jar;/opt/jakarta-regexp/jakarta-regexp-1.2/jakarta-regexp-1.2.jar:/opt/junit/junit3.4/junit.jar;/opt/wls5.1/examples/ejb/basic/statelessSession/build/ejb_basic_statelessSession.jar;/opt/jdk1.3/jre/lib/rt.jar")))
  '(jde-run-option-classpath (quote ("d:/src/grinder/build/classes;e:/junit/junit3.4/junit.jar")));
 
  '(jde-compile-option-directory "/work/src/grinder/build/classes")
