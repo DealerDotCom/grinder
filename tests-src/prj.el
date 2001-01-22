1456b44cf795ff7c6033b9b42c60cc5dcdb532fe
Philip Aston <philipa@mail.com>
2001-01-22 22:11:49 +0000

    How to build and run the tests.

---
+++ tests-src/prj.el
@@ -0,0 +1,9 @@
+
+(jde-set-project-name "Grinder tests")
+(jde-set-variables 
+ '(jde-global-classpath (quote ("/work/src/grinder/build/classes;/opt/junit/junit3.4/junit.jar;/opt/jdk1.3/jre/lib/rt.jar")))
+
+ '(jde-run-option-classpath (quote ("d:/src/grinder/build/tests-classes;d:/src/grinder/build/classes;e:/junit/junit3.4/junit.jar")))
+
+ '(jde-compile-option-directory "/work/src/grinder/build/tests-classes")
+)
