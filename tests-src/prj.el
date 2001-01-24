7ef7b99cef3e00a872a19a8d122b8515d503bd14
Philip Aston <philipa@mail.com>
2001-01-24 22:00:16 +0000

    jde-db-option-classpath.

--- tests-src/prj.el
+++ tests-src/prj.el
@@ -5,5 +5,7 @@
 
  '(jde-run-option-classpath (quote ("d:/src/grinder/build/tests-classes;d:/src/grinder/build/classes;e:/junit/junit3.4/junit.jar")))
 
+ '(jde-db-option-classpath (quote ("d:/src/grinder/build/tests-classes;d:/src/grinder/build/classes;e:/junit/junit3.4/junit.jar")))
+
  '(jde-compile-option-directory "/work/src/grinder/build/tests-classes")
 )
