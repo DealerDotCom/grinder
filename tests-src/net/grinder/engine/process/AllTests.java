bd3a77cbc22f66cca8c30dec64246579b403fb39
Philip Aston <philipa@mail.com>
2001-02-11 18:50:34 +0000

    Fix copyright.

--- tests-src/net/grinder/engine/process/AllTests.java
+++ tests-src/net/grinder/engine/process/AllTests.java
@@ -1,6 +1,6 @@
 // The Grinder
-// Copyright (C) 2001  Paco Gomez
-// Copyright (C) 2001  Philip Aston
+// Copyright (C) 2000, 2001  Paco Gomez
+// Copyright (C) 2000, 2001  Philip Aston
 
 // This program is free software; you can redistribute it and/or
 // modify it under the terms of the GNU General Public License
