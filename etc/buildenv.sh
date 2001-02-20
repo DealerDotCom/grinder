f14a4d61a474425353e31ac686cb0ee48b5a43d9
Philip Aston <philipa@mail.com>
2001-02-20 17:28:26 +0000

    JUnit 3.5.

--- etc/buildenv.sh
+++ etc/buildenv.sh
@@ -3,4 +3,6 @@ export ANT_HOME=$(cygpath -w ${CYGWIN_ANT_HOME})
 export JAVA_HOME=e:\jdk1.3
 export PATH=$PATH:${CYGWIN_ANT_HOME}/bin
 
-export CLASSPATH=$(cygpath -w -p "/opt/jakarta-regexp/jakarta-regexp-1.2/jakarta-regexp-1.2.jar:/opt/junit/junit3.4/junit.jar")
+JUNIT_JAR=/opt/junit/junit3.5/junit.jar
+
+export CLASSPATH=$(cygpath -w -p "/opt/jakarta-regexp/jakarta-regexp-1.2/jakarta-regexp-1.2.jar:${JUNIT_JAR}")
