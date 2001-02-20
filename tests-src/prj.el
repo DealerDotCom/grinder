8bd349551c34e3c02a2294ddaa4622fe929610dc
Philip Aston <philipa@mail.com>
2001-02-20 21:03:21 +0000

    JUnit 3.5.

--- tests-src/prj.el
+++ tests-src/prj.el
@@ -1,11 +1,11 @@
 
 (jde-set-project-name "Grinder tests")
 (jde-set-variables 
- '(jde-global-classpath (quote ("/work/src/grinder/tests-src;/work/src/grinder/build/classes;/opt/junit/junit3.4/junit.jar;/opt/jdk1.3/jre/lib/rt.jar")))
+ '(jde-global-classpath (quote ("/work/src/grinder/tests-src;/work/src/grinder/build/classes;/opt/junit/junit3.5/junit.jar;/opt/jdk1.3/jre/lib/rt.jar")))
 
- '(jde-run-option-classpath (quote ("d:/src/grinder/build/tests-classes;d:/src/grinder/build/classes;e:/junit/junit3.4/junit.jar")))
+ '(jde-run-option-classpath (quote ("d:/src/grinder/build/tests-classes;d:/src/grinder/build/classes;e:/junit/junit3.5/junit.jar")))
 
- '(jde-db-option-classpath (quote ("d:/src/grinder/build/tests-classes;d:/src/grinder/build/classes;e:/junit/junit3.4/junit.jar")))
+ '(jde-db-option-classpath (quote ("d:/src/grinder/build/tests-classes;d:/src/grinder/build/classes;e:/junit/junit3.5/junit.jar")))
 
  '(jde-compile-option-directory "/work/src/grinder/build/tests-classes")
 )
