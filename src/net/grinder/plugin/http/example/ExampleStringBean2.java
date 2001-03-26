889f3bc0d8c001ec3bb97e73d83fe30675fdef47
Philip Aston <philipa@mail.com>
2001-03-26 08:34:56 +0000

    Improve examples.

--- src/net/grinder/plugin/http/example/ExampleStringBean2.java
+++ src/net/grinder/plugin/http/example/ExampleStringBean2.java
@@ -21,19 +21,45 @@ package net.grinder.plugin.http.example;
 
 import net.grinder.plugininterface.PluginException;
 import net.grinder.plugininterface.PluginThreadContext;
+import net.grinder.plugininterface.Test;
 import net.grinder.plugin.http.StringBean;
 
 
 /**
- * Example String Bean.
+ * Example String Bean that implements StringBean.
  * 
  * @author Philip Aston
  * @version $Revision$
  */
-public class ExampleStringBean
+public class ExampleStringBean2 implements StringBean
 {
+    private PluginThreadContext m_pluginThreadContext;
     private int m_count = 0;
 
+    public void initialize(PluginThreadContext pluginThreadContext)
+	throws PluginException
+    {
+	m_pluginThreadContext = pluginThreadContext;
+	m_pluginThreadContext.logMessage("StringBean: initialize");
+    }
+
+    public void beginCycle()
+    {
+	m_pluginThreadContext.logMessage("StringBean: beginCycle");
+	m_count = 0;
+    }
+
+    public boolean doTest(Test test)
+    {
+	m_pluginThreadContext.logMessage("StringBean: doTest");
+	return false;
+    }
+
+    public void endCycle()
+    {
+	m_pluginThreadContext.logMessage("StringBean: endCycle");
+    }
+    
     public String getCount()
     {
 	return Integer.toString(m_count++);
