e9e92e79f65f9138eb2c5ef299eb875d6af01586
Philip Aston <philipa@mail.com>
2001-04-05 21:27:00 +0000

    Paddy Spencer changes:  changed all the members from final, as the proxy needs to call setXxx  methods, which have also been added.

--- src/net/grinder/tools/tcpsniffer/ConnectionDetails.java
+++ src/net/grinder/tools/tcpsniffer/ConnectionDetails.java
@@ -22,11 +22,11 @@ package net.grinder.tools.tcpsniffer;
 
 public class ConnectionDetails
 {
-    private final String m_localHost;
-    private final int m_localPort;
-    private final String m_remoteHost;
-    private final int m_remotePort;
-    private final boolean m_isSecure;
+    private String m_localHost;
+    private int m_localPort;
+    private String m_remoteHost;
+    private int m_remotePort;
+    private boolean m_isSecure;
 
     public ConnectionDetails(String localHost, int localPort,
 			     String remoteHost, int remotePort,
@@ -54,4 +54,40 @@ public class ConnectionDetails
 	    protocol + (m_isSecure ? "s://" : "://" ) +
 	    m_remoteHost + ":" + m_remotePort;
     }
+	
+	public boolean isSecure() {
+		return m_isSecure;
+	}
+	
+	public String getRemoteHost() {
+		return m_remoteHost;
+	}
+	
+	public String getLocalHost() {
+		return m_localHost;
+	}
+
+	public int getRemotePort() {
+		return m_remotePort;
+	}
+	
+	public int getLocalPort() {
+		return m_localPort;
+	}
+
+	public void setRemoteHost(String p) {
+		m_remoteHost = p;
+	}
+	
+	public void setLocalHost(String p) {
+		m_localHost = p;
+	}
+
+	public void setRemotePort(int p) {
+		m_remotePort = p;
+	}
+	
+	public void setLocalPort(int p) {
+		m_localPort = p;
+	}
 }
