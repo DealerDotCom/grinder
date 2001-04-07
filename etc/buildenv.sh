a678a14928c64b03879fe8d05f90075d411dac4d
Philip Aston <philipa@mail.com>
2001-04-07 11:32:06 +0000

    Ant 1.3 groks cygwin - hurrah.

--- etc/buildenv.sh
+++ etc/buildenv.sh
@@ -1,7 +1,6 @@
-CYGWIN_ANT_HOME=/opt/ant/ant1.2
-export ANT_HOME=$(cygpath -w ${CYGWIN_ANT_HOME})
-export JAVA_HOME=e:\jdk1.3
-export PATH=$PATH:${CYGWIN_ANT_HOME}/bin
+export ANT_HOME=/opt/ant/ant1.3
+export JAVA_HOME=//e/jdk1.3
+export PATH=$PATH:${ANT_HOME}/bin
 
 JUNIT_JAR=/opt/junit/junit3.5/junit.jar
 
