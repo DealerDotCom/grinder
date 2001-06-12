04d7cc771f8704ca1c6336e51557633ed1de9c25
Philip Aston <philipa@mail.com>
2001-06-12 11:29:13 +0000

    Tweak.

--- etc/buildenv.sh
+++ etc/buildenv.sh
@@ -2,6 +2,7 @@ export ANT_HOME=/opt/ant/ant1.3
 export JAVA_HOME=//e/jdk1.3
 export PATH=$PATH:${ANT_HOME}/bin
 
-JUNIT_JAR=/opt/junit/junit3.5/junit.jar
+JAKARTA_REGEXP=/opt/jakarta-regexp/jakarta-regexp-1.2/jakarta-regexp-1.2.jar
+JUNIT=/opt/junit/junit3.5/junit.jar
 
-export CLASSPATH=$(cygpath -w -p "/opt/jakarta-regexp/jakarta-regexp-1.2/jakarta-regexp-1.2.jar:${JUNIT_JAR}")
+export CLASSPATH=$(cygpath -w -p "${JAKARTA_REGEXP}:${JUNIT}")
--- etc/setenv.sh
+++ etc/setenv.sh
@@ -1,10 +1,13 @@
 GRINDER_HOME=/work/src/grinder
 
-#GRINDER_JAR=${GRINDER_HOME}/lib/grinder.jar
-GRINDER_JAR=${GRINDER_HOME}/build/classes
-GRINDER_JAR=${GRINDER_JAR}:${GRINDER_HOME}/build/tests-classes # for testing JUnit plugin
+#GRINDER=${GRINDER_HOME}/lib/grinder.jar
+GRINDER=${GRINDER_HOME}/build/classes
+GRINDER=${GRINDER}:${GRINDER_HOME}/build/tests-classes # for testing JUnit plugin
 
-export CLASSPATH=$(cygpath -w -p "${GRINDER_JAR}:/opt/jakarta-regexp/jakarta-regexp-1.2/jakarta-regexp-1.2.jar:/opt/junit/junit3.5/junit.jar")
+JAKARTA_REGEXP=/opt/jakarta-regexp/jakarta-regexp-1.2/jakarta-regexp-1.2.jar
+JUNIT=/opt/junit/junit3.5/junit.jar
+
+export CLASSPATH=$(cygpath -w -p "${GRINDER}:${JAKARTA_REGEXP}:${JUNIT}")
 
 alias grind="java net.grinder.Grinder"
 alias console="java net.grinder.Console"
