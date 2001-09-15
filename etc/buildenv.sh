7abe148c241368edec27563c1ee9179ad9592a47
Philip Aston <philipa@mail.com>
2001-09-15 16:11:43 +0000

    //e is not longer valid.

--- etc/buildenv.sh
+++ etc/buildenv.sh
@@ -1,5 +1,5 @@
 export ANT_HOME=/opt/ant/ant1.3
-export JAVA_HOME=//e/jdk1.3
+export JAVA_HOME=/opt/jdk1.3
 export PATH=$PATH:${ANT_HOME}/bin
 
 JAKARTA_REGEXP=/opt/jakarta-regexp/jakarta-regexp-1.2/jakarta-regexp-1.2.jar
