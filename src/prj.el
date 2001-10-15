84a04f5f5f9cf354cb944f9f090c7218ac9010a9
Philip Aston <philipa@mail.com>
2001-10-15 19:04:05 +0000

    JDE now handles relative paths.

--- src/prj.el
+++ src/prj.el
@@ -1,9 +1,9 @@
 
 (jde-set-project-name "Grinder")
 (jde-set-variables 
- '(jde-global-classpath (quote ("/opt/jdk1.3/jre/lib/rt.jar;/work/src/grinder3/src;/opt/wls5.1/sp6/weblogic510sp6.jar;/opt/wls5.1/lib/weblogicaux.jar;/opt/wls5.1/classes;/opt/wls5.1/license;/opt/jdk1.3/jre/lib/ext/jnet.jar;/opt/jdk1.3/jre/lib/ext/jcert.jar;/opt/jdk1.3/jre/lib/ext/jsse.jar;/opt/jakarta-regexp/jakarta-regexp-1.2/jakarta-regexp-1.2.jar:/opt/junit/junit3.5/junit.jar;/opt/wls5.1/examples/ejb/basic/statelessSession/build/ejb_basic_statelessSession.jar;/opt/bsf/bsf-2_2-jython/build/lib/bsf.jar")))
+ '(jde-global-classpath (quote (".:/opt/jdk1.3/jre/lib/rt.jar:/opt/wls5.1/sp6/weblogic510sp6.jar:/opt/wls5.1/lib/weblogicaux.jar:/opt/wls5.1/classes:/opt/wls5.1/license:/opt/jdk1.3/jre/lib/ext/jnet.jar:/opt/jdk1.3/jre/lib/ext/jcert.jar:/opt/jdk1.3/jre/lib/ext/jsse.jar:/opt/jakarta-regexp/jakarta-regexp-1.2/jakarta-regexp-1.2.jar:/opt/junit/junit3.5/junit.jar:/opt/wls5.1/examples/ejb/basic/statelessSession/build/ejb_basic_statelessSession.jar:/opt/bsf/bsf-2_2-jython/build/lib/bsf.jar")))
 
- '(jde-run-option-classpath (quote ("//d/src/grinder3/build/classes;e:/junit/junit3.5/junit.jar;d:/src/grinder3/build/tests-classes")));
+ '(jde-run-option-classpath (quote ("../build/classes:e:/junit/junit3.5/junit.jar:../build/tests-classes")))
 
  '(jde-compile-option-directory "/work/src/grinder3/build/classes")
 )
--- tests-src/prj.el
+++ tests-src/prj.el
@@ -1,11 +1,11 @@
 
 (jde-set-project-name "Grinder tests")
 (jde-set-variables 
- '(jde-global-classpath (quote ("/work/src/grinder3/tests-src;/work/src/grinder3/build/classes;/opt/junit/junit3.5/junit.jar;/opt/jdk1.3/jre/lib/rt.jar")))
+ '(jde-global-classpath (quote (".:../build/classes:/opt/junit/junit3.5/junit.jar:/opt/jdk1.3/jre/lib/rt.jar")))
 
- '(jde-run-option-classpath (quote ("/cygdrive/d/src/grinder3/build/tests-classes;d:/src/grinder3/build/classes;e:/junit/junit3.5/junit.jar")))
+ '(jde-run-option-classpath (quote ("../build/tests-classes:../build/classes:/opt/junit/junit3.5/junit.jar")))
 
- '(jde-db-option-classpath (quote ("d:/src/grinder3/build/tests-classes;d:/src/grinder3/build/classes;e:/junit/junit3.5/junit.jar")))
+ '(jde-db-option-classpath (quote ("../build/tests-classes:../build/classes:/opt/junit/junit3.5/junit.jar")))
 
- '(jde-compile-option-directory "/work/src/grinder3/build/tests-classes")
+ '(jde-compile-option-directory "../build/tests-classes")
 )
