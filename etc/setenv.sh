cc748c5bc8662c0d0111c390a939f9de4a44b20e
Philip Aston <philipa@mail.com>
2002-04-20 05:31:37 +0000

    JUnit 3.7.

--- etc/setenv.sh
+++ etc/setenv.sh
@@ -4,13 +4,14 @@ GRINDER_HOME=/work/src/grinder
 GRINDER=${GRINDER_HOME}/build/classes
 GRINDER=${GRINDER}:${GRINDER_HOME}/build/tests-classes # for testing JUnit plugin
 
+JAKARTA_ORO=/opt/jakarta-oro/jakarta-oro-2.0.6/jakarta-oro-2.0.6.jar
 JAKARTA_REGEXP=/opt/jakarta-regexp/jakarta-regexp-1.2/jakarta-regexp-1.2.jar
-JUNIT=/opt/junit/junit3.5/junit.jar
+JUNIT=/opt/junit/junit3.7/junit.jar
 
 JTIDY=/opt/jtidy/jtidy-04aug2000r7-dev/build/Tidy.jar
 XALAN=/opt/xalan/xalan-j_2_3_1/bin/xalan.jar:/opt/xalan/xalan-j_2_3_1/bin/xml-apis.jar
 
-export CLASSPATH=$(cygpath -w -p "${GRINDER}:${JAKARTA_REGEXP}:${JUNIT}:${JTIDY}:${XALAN}")
+export CLASSPATH=$(cygpath -w -p "${GRINDER}:${JAKARTA_ORO}ht:${JAKARTA_REGEXP}:${JUNIT}:${JTIDY}:${XALAN}")
 
 alias grind="java net.grinder.Grinder"
 alias console="java net.grinder.Console"
