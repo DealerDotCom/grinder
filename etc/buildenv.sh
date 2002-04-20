0a25fdb7239cd80b8346b80aa3f8115c60452985
Philip Aston <philipa@mail.com>
2002-04-20 10:27:48 +0000

    ORO. JUnit 3.7.

--- etc/buildenv.sh
+++ etc/buildenv.sh
@@ -3,9 +3,10 @@ export JAVA_HOME=/opt/jdk1.3.1_02
 export PATH=$PATH:${ANT_HOME}/bin
 
 J2EE=/opt/bea/wlserver6.1/lib/weblogic.jar
+JAKARTA_ORO=/opt/jakarta-oro/jakarta-oro-2.0.6/jakarta-oro-2.0.6.jar
 JAKARTA_REGEXP=/opt/jakarta-regexp/jakarta-regexp-1.2/jakarta-regexp-1.2.jar
 JTIDY=/opt/jtidy/jtidy-04aug2000r7-dev/build/Tidy.jar
-JUNIT=/opt/junit/junit3.5/junit.jar
+JUNIT=/opt/junit/junit3.7/junit.jar
 XALAN=/opt/xalan/xalan-j_2_3_1/bin/xalan.jar:/opt/xalan/xalan-j_2_3_1/bin/xml-apis.jar
 
-export CLASSPATH=$(cygpath -w -p "${JAKARTA_REGEXP}:${JUNIT}:${J2EE}:${JTIDY}:${XALAN}")
+export CLASSPATH=$(cygpath -w -p "${JAKARTA_ORO}:${JAKARTA_REGEXP}:${JUNIT}:${J2EE}:${JTIDY}:${XALAN}")
