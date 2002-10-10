a1ddd1872fd55ab4bb453c7fb974579230d3e0f7
Philip Aston <philipa@mail.com>
2002-10-10 07:12:16 +0000

    Ship Jython.

--- etc/setenv.sh
+++ etc/setenv.sh
@@ -1,15 +1,14 @@
 GRINDER_HOME=/work/src/grinder3
 
 #GRINDER=${GRINDER_HOME}/lib/grinder.jar
-GRINDER=${GRINDER_HOME}/build/classes:${GRINDER_HOME}/lib/jakarta-oro-2.0.6.jar
+GRINDER=${GRINDER_HOME}/build/classes:${GRINDER_HOME}/lib/jython.jar
 GRINDER=${GRINDER_HOME}/build/tests-classes:${GRINDER} # for testing JUnit plugin
 
-JYTHON=/opt/jython/jython-2.1/jython.jar
 JUNIT=/opt/junit/junit3.7/junit.jar
 JTIDY=/opt/jtidy/jtidy-04aug2000r7-dev/build/Tidy.jar
 XALAN=/opt/xalan/xalan-j_2_3_1/bin/xalan.jar:/opt/xalan/xalan-j_2_3_1/bin/xml-apis.jar
 
-export CLASSPATH=$(cygpath -w -p "${GRINDER}:${JUNIT}:${JTIDY}:${JYTHON}")
+export CLASSPATH=$(cygpath -w -p "${GRINDER}:${JUNIT}:${JTIDY}")
 
 alias grind="java net.grinder.Grinder"
 alias console="java net.grinder.Console"
