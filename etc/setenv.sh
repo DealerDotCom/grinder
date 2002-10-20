be55bf7a4adc3b3e93742515c684f36e254fb01c
Philip Aston <philipa@mail.com>
2002-10-20 16:03:48 +0000

    Add WLS example client classpath.

--- etc/setenv.sh
+++ etc/setenv.sh
@@ -7,7 +7,10 @@ GRINDER=${GRINDER_HOME}/build/tests-classes:${GRINDER} # for testing JUnit plugi
 JUNIT=/opt/junit/junit3.7/junit.jar
 XALAN=/opt/xalan/xalan-j_2_3_1/bin/xalan.jar:/opt/xalan/xalan-j_2_3_1/bin/xml-apis.jar
 
-export CLASSPATH=$(cygpath -w -p "${GRINDER}:${JUNIT}:${JTIDY}")
+WLS=/opt/bea/weblogic700/server/lib/weblogic.jar
+WLS_EXAMPLES=/opt/bea/weblogic700/samples/server/stage/examples/clientclasses/ejb20_basic_statefulSession_client.jar
+
+export CLASSPATH=$(cygpath -w -p "${GRINDER}:${JUNIT}:${JTIDY}:${WLS}:${WLS_EXAMPLES}")
 
 alias grind="java net.grinder.Grinder"
 alias console="java net.grinder.Console"
