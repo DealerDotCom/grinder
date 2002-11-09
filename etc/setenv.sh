3a4ab27226b90aeed83f1734b69d243e9efdb8ee
Philip Aston <philipa@mail.com>
2002-11-09 13:59:26 +0000

    Go back to shipping our own version of Jakarta ORO. The one packaged with Jython is ancient and does not correctly cope with multiline matches.

--- etc/setenv.sh
+++ etc/setenv.sh
@@ -1,17 +1,21 @@
 GRINDER_HOME=/work/src/grinder3
 
 #GRINDER=${GRINDER_HOME}/lib/grinder.jar
-GRINDER=${GRINDER_HOME}/build/classes:${GRINDER_HOME}/lib/jython.jar
+GRINDER=${GRINDER_HOME}/build/classes:${GRINDER_HOME}/lib/jakarta-oro-2.0.6.jar:${GRINDER_HOME}/lib/jython.jar
 GRINDER=${GRINDER_HOME}/build/tests-classes:${GRINDER} # for testing JUnit plugin
 
 JUNIT=/opt/junit/junit3.7/junit.jar
 XALAN=/opt/xalan/xalan-j_2_3_1/bin/xalan.jar:/opt/xalan/xalan-j_2_3_1/bin/xml-apis.jar
 
 WLS=/opt/bea/weblogic700/server/lib/weblogic.jar
-WLS_EXAMPLES=/opt/bea/weblogic700/samples/server/stage/examples/clientclasses/ejb20_basic_statefulSession_client.jar
+#WLS=/opt/bea/weblogic700/server/lib/webserviceclient.jar
+WLS_EXAMPLES_DIR=/opt/bea/weblogic700/samples/server/stage/examples/clientclasses/
+WLS_EXAMPLES=${WLS_EXAMPLES_DIR}/ejb20_basic_statefulSession_client.jar:${WLS_EXAMPLES_DIR}/HelloWorld_client.jar
 
-export CLASSPATH=$(cygpath -w -p "${GRINDER}:${JUNIT}:${JTIDY}:${WLS}:${WLS_EXAMPLES}")
+export CLASSPATH=$(cygpath -w -p "${GRINDER}:${JUNIT}:${WLS}:${WLS_EXAMPLES}")
 
 alias grind="java net.grinder.Grinder"
 alias console="java net.grinder.Console"
 alias sniff="java net.grinder.TCPProxy"
+
+# My amazon license D1JM5WG5VP8KO1
