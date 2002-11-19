5c6ab2d4ad215a96ad07b8e11731c11db400192a
Philip Aston <philipa@mail.com>
2002-11-19 21:47:29 +0000

    Make immutable. Add connection identity string.

--- src/net/grinder/tools/tcpproxy/ConnectionDetails.java
+++ src/net/grinder/tools/tcpproxy/ConnectionDetails.java
@@ -29,15 +29,16 @@ package net.grinder.tools.tcpproxy;
  * @author <a href="mailto:paston@bea.com">Philip Aston</a>
  * @version 1.0
  */
-public class ConnectionDetails
+public final class ConnectionDetails
 {
     private final int m_hashCode;
 
-    private String m_localHost;
-    private int m_localPort;
-    private String m_remoteHost;
-    private int m_remotePort;
-    private boolean m_isSecure;
+    private final String m_localHost;
+    private final int m_localPort;
+    private final String m_remoteHost;
+    private final int m_remotePort;
+    private final boolean m_isSecure;
+    private final String m_connectionIdentity;
 
     /**
      * Creates a new <code>ConnectionDetails</code> instance.
@@ -47,6 +48,7 @@ public class ConnectionDetails
      * @param remoteHost a <code>String</code> value
      * @param remotePort an <code>int</code> value
      * @param isSecure a <code>boolean</code> value
+     * @throws IllegalArgumentException If local and remote details are the same.
      */
     public ConnectionDetails(String localHost, int localPort,
 			     String remoteHost, int remotePort,
@@ -64,6 +66,34 @@ public class ConnectionDetails
 	    m_localPort ^
 	    m_remotePort ^
 	    (m_isSecure ? 0x55555555 : 0);
+
+	final int c = localHost.compareTo(remoteHost);
+	final boolean localIsLeast;
+
+	if (c == 0) {
+	    if (localPort == remotePort) {
+		throw new IllegalArgumentException(
+		    "Local and remote sockets are the same");
+	    }
+	    
+	    localIsLeast = localPort < remotePort;
+	}
+	else {
+	    localIsLeast = c < 0;
+	}
+
+	if (localIsLeast) {
+	    m_connectionIdentity =
+		localHost + "|" + localPort + "|" +
+		remoteHost + "|" + remotePort + "|" +
+		isSecure;
+	}
+	else {
+	    m_connectionIdentity =
+		remoteHost + "|" + remotePort + "|" +
+		localHost + "|" + localPort + "|" +
+		isSecure;
+	}
     }
 
     /**
@@ -144,46 +174,6 @@ public class ConnectionDetails
     }
 
     /**
-     * Mutator.
-     *
-     * @param p a <code>String</code> value
-     */
-    public void setRemoteHost(String p)
-    {
-	m_remoteHost = p;
-    }
-
-    /**
-     * Mutator.
-     *
-     * @param p a <code>String</code> value
-     */
-    public void setLocalHost(String p)
-    {
-	m_localHost = p;
-    }
-
-    /**
-     * Mutator.
-     *
-     * @param p an <code>int</code> value
-     */
-    public void setRemotePort(int p)
-    {
-	m_remotePort = p;
-    }
-
-    /**
-     * Mutator.
-     *
-     * @param p an <code>int</code> value
-     */
-    public void setLocalPort(int p)
-    {
-	m_localPort = p;
-    }
-
-    /**
      * Value based equality.
      *
      * @param other an <code>Object</code> value
@@ -220,5 +210,29 @@ public class ConnectionDetails
     public final int hashCode()
     {
 	return m_hashCode;
+    }
+
+    /**
+     * Return a <code>String</code> that represents the connection.
+     * <code>ConnectionDetails</code> representing either end of the
+     * same connection always return the same thing.
+     *
+     * @return Represents the connection.
+     */
+    public final String getConnectionIdentity() 
+    {
+	return m_connectionIdentity;
+    }
+
+    /**
+     * Return a <code>ConnectionDetails</code> representing the other end of the connection.
+     *
+     * @return The other end of the connection to this.
+     */
+    public final ConnectionDetails getOtherEnd() 
+    {
+	return new ConnectionDetails(getRemoteHost(), getRemotePort(),
+				     getLocalHost(), getLocalPort(),
+				     isSecure());
     }
 }
