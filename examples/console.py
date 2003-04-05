17f3bacff6d08d9a2ca0bdfbaaf2e37e4b003338
Philip Aston <philipa@mail.com>
2003-04-05 11:57:28 +0000

    Console test script.

---
+++ examples/console.py
@@ -0,0 +1,25 @@
+# Test script which generates some random data for testing the
+# console.
+
+from net.grinder.script import Test
+from java.util import Random
+
+r = Random()
+
+def doIt():
+    grinder.sleep(r.nextInt(100));
+    pass
+
+tests = [ Test(i, "Test %s" % i).wrap(doIt) for i in range(0, 100) ]
+
+class TestRunner:
+    def __call__(self):
+        statistics = grinder.statistics
+        statistics.delayReports = 1
+        
+        for test in tests:
+            test()
+            if r.nextInt() % 500 == 0:
+                statistics.success = 0
+
+        grinder.sleep(100)
