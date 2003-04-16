98c25435f4d82d238322e724ced73f57b2be240c
Philip Aston <philipa@mail.com>
2003-04-16 18:39:30 +0000

    Hmmm.. better have the JSSE then.

--- etc/buildenv.sh
+++ etc/buildenv.sh
@@ -5,6 +5,8 @@ export PATH=${ANT_HOME}/bin:${JAVA_HOME}/bin:$PATH
 
 CHECKSTYLE=/opt/checkstyle/checkstyle-2.1/checkstyle-all-2.1.jar
 JAKARTA_REGEXP=/opt/jakarta-regexp/jakarta-regexp-1.2/jakarta-regexp-1.2.jar
+JSSE_HOME=/opt/jsse/jsse1.0.2/lib/
+JSSE=${JSSE_HOME}/jsse.jar:${JSSE_HOME}/jnet.jar:${JSSE_HOME}/jcert.jar
 JUNIT=/opt/junit/junit3.7/junit.jar
 
-export CLASSPATH=$(cygpath -w -p "${JUNIT}:${CHECKSTYLE}")
+export CLASSPATH=$(cygpath -w -p "${JUNIT}:${CHECKSTYLE}:${JSSE}")
