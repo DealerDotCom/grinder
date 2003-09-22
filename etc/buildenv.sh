872085a02dbedcee7499d2abc33940928724e3b0
Philip Aston <philipa@mail.com>
2003-09-22 18:33:46 +0000

    Fix for latest environment.

--- etc/buildenv.sh
+++ etc/buildenv.sh
@@ -1,6 +1,5 @@
 export ANT_HOME=/opt/ant/jakarta-ant-1.5.1
-#export JAVA_HOME=/system/bea-8.1b/jdk141_02
-export JAVA_HOME=/system/bea-7.0.2.0/jdk131_06
+export JAVA_HOME=/opt/jdk1.3.1_02/
 export PATH=${ANT_HOME}/bin:${JAVA_HOME}/bin:$PATH
 
 CHECKSTYLE=/opt/checkstyle/checkstyle-2.1/checkstyle-all-2.1.jar
