7d4eb887b97d9befe418e0856fe491add5188fb4
Philip Aston <philipa@mail.com>
2003-10-01 17:36:22 +0000

    Fix checkstyle zealotry.

--- src/net/grinder/communication/SocketSet.java
+++ src/net/grinder/communication/SocketSet.java
@@ -25,6 +25,7 @@ import java.io.BufferedInputStream;
 import java.io.InputStream;
 import java.io.IOException;
 import java.io.ObjectInputStream;
+import java.net.Socket;
 import java.util.ArrayList;
 import java.util.Iterator;
 import java.util.List;
