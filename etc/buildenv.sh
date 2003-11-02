5d12087390b26a197d00f80042f8a1245540a4b2
Philip Aston <philipa@mail.com>
2003-11-02 17:06:14 +0000

    Incorporate Checkstyle 3.1 into the build.

--- etc/buildenv.sh
+++ etc/buildenv.sh
@@ -2,7 +2,7 @@ export ANT_HOME=/opt/ant/jakarta-ant-1.5.1
 export JAVA_HOME=/opt/jdk1.3.1_02/
 export PATH=${ANT_HOME}/bin:${JAVA_HOME}/bin:$PATH
 
-CHECKSTYLE=/opt/checkstyle/checkstyle-2.1/checkstyle-all-2.1.jar
+CHECKSTYLE=/opt/checkstyle/checkstyle-3.1/checkstyle-all-3.1.jar
 JAKARTA_REGEXP=/opt/jakarta-regexp/jakarta-regexp-1.2/jakarta-regexp-1.2.jar
 JSSE_HOME=/opt/jsse/jsse1.0.2/lib/
 JSSE=${JSSE_HOME}/jsse.jar:${JSSE_HOME}/jnet.jar:${JSSE_HOME}/jcert.jar
