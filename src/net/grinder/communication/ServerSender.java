61f126685b0a4b4bc428a771224c19482b46f1ad
Philip Aston <philipa@mail.com>
2003-12-16 18:55:22 +0000

    ServerSender implementation.

---
+++ src/net/grinder/communication/ServerSender.java
@@ -0,0 +1,173 @@
+// Copyright (C) 2000, 2001, 2002, 2003 Philip Aston
+// All rights reserved.
+//
+// This file is part of The Grinder software distribution. Refer to
+// the file LICENSE which is part of The Grinder distribution for
+// licensing details. The Grinder distribution is available on the
+// Internet at http://grinder.sourceforge.net/
+//
+// THIS SOFTWARE IS PROVIDED BY THE COPYRIGHT HOLDERS AND CONTRIBUTORS
+// "AS IS" AND ANY EXPRESS OR IMPLIED WARRANTIES, INCLUDING, BUT NOT
+// LIMITED TO, THE IMPLIED WARRANTIES OF MERCHANTABILITY AND FITNESS
+// FOR A PARTICULAR PURPOSE ARE DISCLAIMED. IN NO EVENT SHALL THE
+// REGENTS OR CONTRIBUTORS BE LIABLE FOR ANY DIRECT, INDIRECT,
+// INCIDENTAL, SPECIAL, EXEMPLARY, OR CONSEQUENTIAL DAMAGES
+// (INCLUDING, BUT NOT LIMITED TO, PROCUREMENT OF SUBSTITUTE GOODS OR
+// SERVICES; LOSS OF USE, DATA, OR PROFITS; OR BUSINESS INTERRUPTION)
+// HOWEVER CAUSED AND ON ANY THEORY OF LIABILITY, WHETHER IN CONTRACT,
+// STRICT LIABILITY, OR TORT (INCLUDING NEGLIGENCE OR OTHERWISE)
+// ARISING IN ANY WAY OUT OF THE USE OF THIS SOFTWARE, EVEN IF ADVISED
+// OF THE POSSIBILITY OF SUCH DAMAGE.
+
+package net.grinder.communication;
+
+import java.io.IOException;
+import java.io.ObjectOutputStream;
+import java.util.Iterator;
+
+
+/**
+ * Manages the sending of messages to many clients.
+ *
+ * @author Philip Aston
+ * @version $Revision$
+ */
+public final class ServerSender extends AbstractSender {
+
+  private final Acceptor m_acceptor;
+  private final ThreadSafeQueue m_workQueue = new ThreadSafeQueue();
+
+  /**
+   * Factory method that creates a <code>ServerSender</code> that
+   * listens on the given address.
+   *
+   * @param addressString The TCP address to listen on. Zero-length
+   * string => listen on all interfaces.
+   * @param port The TCP port to listen to. 0 => any local port.
+   * @return The ServerSender.
+   * @throws CommunicationException If server socket could not be
+   * bound.
+   */
+  public static ServerSender bindTo(String addressString, int port)
+    throws CommunicationException {
+
+    return new ServerSender(new Acceptor(addressString, port), 3);
+  }
+
+  /**
+   * Constructor.
+   *
+   * @param acceptor Acceptor that manages connections to our server socket.
+   * @param numberOfThreads Number of sender threads to use.
+   * @throws CommunicationException If server socket could not be
+   * bound.
+   */
+  ServerSender(Acceptor acceptor, int numberOfThreads)
+    throws CommunicationException {
+
+    m_acceptor = acceptor;
+
+    final ThreadGroup threadGroup = m_acceptor.getThreadGroup();
+
+    for (int i = 0; i < numberOfThreads; ++i) {
+      new SenderThread(threadGroup, i).start();
+    }
+  }
+
+  /**
+   * Send a message.
+   *
+   * @param message The message.
+   * @exception IOException If an error occurs.
+   */
+  protected void writeMessage(Message message) throws IOException {
+
+    try {
+      final Iterator iterator =
+        m_acceptor.getSocketSet().reserveAllHandles().iterator();
+
+      while (iterator.hasNext()) {
+        m_workQueue.queue(
+          new MessageHandlePair(message, (SocketSet.Handle) iterator.next()));
+      }
+    }
+    catch (ThreadSafeQueue.ShutdownException e) {
+      // Assertion failure.
+      throw new RuntimeException("MessageQueue unexpectedly shutdown");
+    }
+    catch (InterruptedException e) {
+      // Assertion failure.
+      throw new RuntimeException("Unexpectedly shutdown");
+    }
+  }
+
+  /**
+   * Shut down this reciever.
+   *
+   * @throws CommunicationException If an IO exception occurs.
+   */
+  public void shutdown() throws CommunicationException {
+
+    m_workQueue.shutdown();
+
+    // Will also shut down our SenderThreads.
+    m_acceptor.shutdown();
+  }
+
+  private final class SenderThread extends Thread {
+
+    public SenderThread(ThreadGroup threadGroup, int senderThreadIndex) {
+      super(threadGroup, "Sender thread " + senderThreadIndex);
+      setDaemon(true);
+    }
+
+    public void run() {
+
+      try {
+        while (true) {
+          final MessageHandlePair pair =
+            (MessageHandlePair) m_workQueue.dequeue(true);
+
+          final Message message = pair.getMessage();
+          final SocketSet.Handle socketHandle = pair.getHandle();
+
+          try {
+            final ObjectOutputStream objectStream =
+              new ObjectOutputStream(socketHandle.getOutputStream());
+
+            objectStream.writeObject(message);
+            objectStream.flush();
+          }
+          catch (IOException e) {
+            socketHandle.close();
+            //            m_messageQueue.queue(e);
+          }
+          finally {
+            socketHandle.free();
+          }
+        }
+      }
+      catch (ThreadSafeQueue.ShutdownException e) {
+        // We've been shutdown, exit this thread.
+      }
+    }
+  }
+
+  private static final class MessageHandlePair {
+    private final Message m_message;
+    private final SocketSet.Handle m_handle;
+
+    public MessageHandlePair(Message message, SocketSet.Handle handle) {
+      m_message = message;
+      m_handle = handle;
+    }
+
+    public Message getMessage() {
+      return m_message;
+    }
+
+    public SocketSet.Handle getHandle() {
+      return m_handle;
+    }
+  }
+}
