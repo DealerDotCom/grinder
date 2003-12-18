5c64e22ac359d6bcc52d4d1798a9ac36172df133
Philip Aston <philipa@mail.com>
2003-12-18 18:43:17 +0000

    Test ClientSender.

---
+++ tests-src/net/grinder/communication/TestClientSender.java
@@ -0,0 +1,223 @@
+// Copyright (C) 2003 Philip Aston
+// All rights reserved.
+//
+// This file is part of The Grinder software distribution. Refer to
+// the file LICENSE which is part of The Grinder distribution for
+// licensing details. The Grinder distribution is available on the
+// Internet at http://grinder.sourceforge.net/
+//
+// THIS SOFTWARE IS PROVIDED BY THE COPYRIGHT HOLDERS AND CONTRIBUTORS
+// "AS IS" AND ANY EXPRESS OR IMPLIED WARRANTIES, INCLUDING, BUT NOT
+// LIMITED TO, THE IMPLIED WARRANTIES OF MERCHANTABILITY AND FITNESS
+// FOR A PARTICULAR PURPOSE ARE DISCLAIMED. IN NO EVENT SHALL THE
+// REGENTS OR CONTRIBUTORS BE LIABLE FOR ANY DIRECT, INDIRECT,
+// INCIDENTAL, SPECIAL, EXEMPLARY, OR CONSEQUENTIAL DAMAGES
+// (INCLUDING, BUT NOT LIMITED TO, PROCUREMENT OF SUBSTITUTE GOODS OR
+// SERVICES; LOSS OF USE, DATA, OR PROFITS; OR BUSINESS INTERRUPTION)
+// HOWEVER CAUSED AND ON ANY THEORY OF LIABILITY, WHETHER IN CONTRACT,
+// STRICT LIABILITY, OR TORT (INCLUDING NEGLIGENCE OR OTHERWISE)
+// ARISING IN ANY WAY OUT OF THE USE OF THIS SOFTWARE, EVEN IF ADVISED
+// OF THE POSSIBILITY OF SUCH DAMAGE.
+
+package net.grinder.communication;
+
+import java.io.ByteArrayInputStream;
+import java.io.ByteArrayOutputStream;
+import java.io.InputStream;
+import java.io.IOException;
+import java.io.ObjectInputStream;
+import java.io.ObjectOutputStream;
+import java.io.OutputStream;
+import java.net.InetAddress;
+import java.net.ServerSocket;
+import java.net.Socket;
+
+import junit.framework.TestCase;
+
+
+/**
+ *  Unit test case for <code>ClientSender</code>.
+ *
+ * @author Philip Aston
+ * @version $Revision$
+ */
+public class TestClientSender extends TestCase {
+
+  public TestClientSender(String name) {
+    super(name);
+  }
+
+  public void testSendWithStreams() throws Exception {
+
+    final ByteArrayOutputStream byteOutputStream = new ByteArrayOutputStream();
+
+    final ClientSender clientSender =
+      new ClientSender((OutputStream)byteOutputStream);
+
+    final SimpleMessage message1 = new SimpleMessage();
+    final SimpleMessage message2 = new SimpleMessage();
+
+    // Stream constructor doesn't support message initialisation.
+    try {
+      clientSender.send(message1);
+      fail("Expected CommunicationException");
+    }
+    catch (CommunicationException e) {
+    }
+    
+    message1.setSenderInformation("Test", getClass().getName(), 1);
+    message2.setSenderInformation("Test", getClass().getName(), 2);
+
+    clientSender.send(message1);
+    clientSender.send(message2);
+    
+    final ByteArrayInputStream byteInputStream =
+      new ByteArrayInputStream(byteOutputStream.toByteArray());
+
+    // Need an ObjectInputStream for every message. See note in
+    // ClientSender.writeMessage.
+    final ObjectInputStream inputStream1 =
+      new ObjectInputStream(byteInputStream);
+    final Object o1 = inputStream1.readObject();
+
+    final ObjectInputStream inputStream2 =
+      new ObjectInputStream(byteInputStream);
+    final Object o2 = inputStream2.readObject();
+
+    assertEquals(message1, o1);
+    assertTrue(message1.payloadEquals((Message) o1));
+
+    assertEquals(message2, o2);
+    assertTrue(message2.payloadEquals((Message) o2));
+
+    assertEquals(0, byteInputStream.available());
+  }
+
+  public void testShutdownWithStreams() throws Exception {
+
+    final ByteArrayOutputStream byteOutputStream = new ByteArrayOutputStream();
+
+    final ClientSender clientSender =
+      new ClientSender((OutputStream)byteOutputStream);
+
+    final Message message = new SimpleMessage();
+    message.setSenderInformation("Test", getClass().getName(), 99);
+
+    clientSender.send(message);
+
+    clientSender.shutdown();
+
+    try {
+      clientSender.send(message);
+      fail("Expected CommunicationException");
+    }
+    catch (CommunicationException e) {
+    }
+  }
+
+  private class SocketAcceptor implements Runnable {
+    
+    private final ServerSocket m_serverSocket;
+    private Exception m_exception;
+    private Socket m_lastAcceptedSocket;
+
+    public SocketAcceptor() throws Exception {
+      m_serverSocket = new ServerSocket(0);
+    }
+
+    public void run() {
+      try {
+        m_lastAcceptedSocket = m_serverSocket.accept();
+      }
+      catch (Exception e) {
+        m_exception = e;
+      }
+    }
+
+    public int getLocalPort() {
+      return m_serverSocket.getLocalPort();
+    }
+
+    public Socket getLastAcceptedSocket() {
+      return m_lastAcceptedSocket;
+    }
+
+    public Exception getException() {
+      return m_exception;
+    }
+  }
+
+  public void testSendWithSockets() throws Exception {
+
+    final SocketAcceptor socketAcceptor = new SocketAcceptor();
+
+    final Thread acceptorThread = new Thread(socketAcceptor);
+    acceptorThread.start();
+
+    final ClientSender clientSender =
+      ClientSender.connectTo("Test", InetAddress.getLocalHost().getHostName(),
+                             socketAcceptor.getLocalPort());
+
+    final SimpleMessage message1 = new SimpleMessage();
+    final SimpleMessage message2 = new SimpleMessage();
+
+    // A socket ClientSender should be able to both route messages
+    // from other Senders and originate their own.
+    message1.setSenderInformation("Grinder ID", getClass().getName(), 1);
+
+    clientSender.send(message1);
+    clientSender.send(message2);
+
+    assertEquals("Grinder ID", message1.getSenderGrinderID());
+    assertEquals("Test", message2.getSenderGrinderID());
+
+    acceptorThread.join();
+    
+    final InputStream socketInput =
+      socketAcceptor.getLastAcceptedSocket().getInputStream();
+
+    // Need an ObjectInputStream for every message. See note in
+    // ClientSender.writeMessage.
+    final ObjectInputStream inputStream1 = new ObjectInputStream(socketInput);
+    final Object o1 = inputStream1.readObject();
+
+    final ObjectInputStream inputStream2 = new ObjectInputStream(socketInput);
+    final Object o2 = inputStream2.readObject();
+
+    assertEquals(message1, o1);
+    assertTrue(message1.payloadEquals((Message) o1));
+
+    assertEquals(message2, o2);
+    assertTrue(message2.payloadEquals((Message) o2));
+
+    assertEquals(0, socketInput.available());
+  }
+
+  public void testShutdownWithSockets() throws Exception {
+
+    final SocketAcceptor socketAcceptor = new SocketAcceptor();
+
+    final Thread acceptorThread = new Thread(socketAcceptor);
+    acceptorThread.start();
+
+    final ClientSender clientSender =
+      ClientSender.connectTo("Test", InetAddress.getLocalHost().getHostName(),
+                             socketAcceptor.getLocalPort());
+
+    final Message message = new SimpleMessage();
+    message.setSenderInformation("Test", getClass().getName(), 99);
+
+    clientSender.send(message);
+
+    clientSender.shutdown();
+
+    try {
+      clientSender.send(message);
+      fail("Expected CommunicationException");
+    }
+    catch (CommunicationException e) {
+    }
+
+    acceptorThread.join();
+  }
+}
