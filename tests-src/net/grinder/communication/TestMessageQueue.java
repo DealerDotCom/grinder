9f93064925ec5f318b1a351f2ebe5aedfe4a8e1f
Philip Aston <philipa@mail.com>
2003-12-18 08:31:15 +0000

    Update.

--- tests-src/net/grinder/communication/TestMessageQueue.java
+++ tests-src/net/grinder/communication/TestMessageQueue.java
@@ -1,5 +1,4 @@
-// Copyright (C) 2000 Paco Gomez
-// Copyright (C) 2000, 2001, 2002 Philip Aston
+// Copyright (C) 2000, 2001, 2002, 2003 Philip Aston
 // All rights reserved.
 //
 // This file is part of The Grinder software distribution. Refer to
@@ -23,8 +22,6 @@
 package net.grinder.communication;
 
 import junit.framework.TestCase;
-import junit.swingui.TestRunner;
-//import junit.textui.TestRunner;
 
 import java.util.LinkedList;
 import java.util.List;
@@ -37,252 +34,195 @@ import java.util.Random;
  * @author Philip Aston
  * @version $Revision$
  **/
-public class TestMessageQueue extends TestCase
-{
-    public static void main(String[] args)
-    {
-	TestRunner.run(TestMessageQueue.class);
+public class TestMessageQueue extends TestCase {
+
+  public TestMessageQueue(String name) {
+    super(name);
+  }
+
+  private final MessageQueue m_queue = new MessageQueue(false);
+
+  public void testWithOneThread() throws Exception {
+    final Message[] messages = {
+      new SimpleMessage(10),
+      new SimpleMessage(0),
+      new SimpleMessage(999),
+    };
+
+    for (int i=0; i<messages.length; ++i) {
+      m_queue.queue(messages[i]);
     }
 
-    public TestMessageQueue(String name)
-    {
-	super(name);
+    for (int i=0; i<messages.length; ++i) {
+      assertSame(messages[i], m_queue.dequeue(false));
     }
-
-    private final MessageQueue m_queue = new MessageQueue(false);
-
-    protected void setUp() throws Exception
-    {
-    }
-
-    public void testWithOneThread() throws Exception
-    {
-	final Message[] messages = {
-	    new SimpleMessage(10),
-	    new SimpleMessage(0),
-	    new SimpleMessage(999),
-	};
-
-	for (int i=0; i<messages.length; ++i) {
-	    m_queue.queue(messages[i]);
-	}
-
-	for (int i=0; i<messages.length; ++i) {
-	    assertSame(messages[i], m_queue.dequeue(false));
-	}
 	
-	assertNull(m_queue.dequeue(false));
-    }
+    assertNull(m_queue.dequeue(false));
+  }
     
-    public void testWithActiveDequeuer() throws Exception
-    {
-	final Message[] messages = {
-	    new SimpleMessage(10),
-	    new SimpleMessage(0),
-	    new SimpleMessage(999),
-	};
+  public void testWithActiveDequeuer() throws Exception {
+    final Message[] messages = {
+      new SimpleMessage(10),
+      new SimpleMessage(0),
+      new SimpleMessage(999),
+    };
 
-	final DequeuerThread dequeuerThread = 
-	    new DequeuerThread(messages.length);
-	dequeuerThread.start();
+    final DequeuerThread dequeuerThread = new DequeuerThread(messages.length);
+    dequeuerThread.start();
 
-	for (int i=0; i<messages.length; ++i) {
-	    m_queue.queue(messages[i]);
-	}
-
-	dequeuerThread.join();
-
-	final List receivedMessages = (List)dequeuerThread.getMessages();
-
-	assertEquals(messages.length, receivedMessages.size());
-
-	for (int i=0; i<messages.length; ++i) {
-	    assertSame(messages[i], receivedMessages.get(i));
-	}
+    for (int i=0; i<messages.length; ++i) {
+      m_queue.queue(messages[i]);
     }
 
-    public void testShutdownReciever() throws Exception
-    {
-	final DequeuerThread dequeuerThread = new DequeuerThread(1);
-	dequeuerThread.start();
-	m_queue.shutdown();
+    dequeuerThread.join();
 
-	dequeuerThread.join();
-	assertTrue(dequeuerThread.getException() instanceof
-		   MessageQueue.ShutdownException);
+    final List receivedMessages = (List)dequeuerThread.getMessages();
 
-	try {
-	    m_queue.queue(new SimpleMessage(0));
-	    fail("Expected a ShutdownException");
-	}
-	catch (MessageQueue.ShutdownException e) {
-	}
+    assertEquals(messages.length, receivedMessages.size());
 
-	try {
-	    m_queue.dequeue(true);
-	    fail("Expected a ShutdownException");
-	}
-	catch (MessageQueue.ShutdownException e) {
-	}
+    for (int i=0; i<messages.length; ++i) {
+      assertSame(messages[i], receivedMessages.get(i));
+    }
+  }
+
+  public void testShutdownReciever() throws Exception {
+
+    final DequeuerThread dequeuerThread = new DequeuerThread(1);
+    dequeuerThread.start();
+    m_queue.shutdown();
+
+    dequeuerThread.join();
+    assertTrue(dequeuerThread.getException() instanceof
+      ThreadSafeQueue.ShutdownException);
+
+    try {
+      m_queue.queue(new SimpleMessage(0));
+      fail("Expected a ShutdownException");
+    }
+    catch (ThreadSafeQueue.ShutdownException e) {
     }
 
-    public void testManyQueuersAndDequeuers() throws Exception
-    {
-	final Thread[] queuers = new Thread[6];
-	final Thread[] dequeuers = new Thread[3];
-	final Random random = new Random();
+    try {
+      m_queue.dequeue(true);
+      fail("Expected a ShutdownException");
+    }
+    catch (ThreadSafeQueue.ShutdownException e) {
+    }
+  }
 
-	for (int i=0; i<queuers.length; ++i) {
-	    queuers[i] = new Thread() {
-		    public void run() {
-			for (int j=0; j<10; ++j) {
-			    try {
-				m_queue.queue(new SimpleMessage(0));
-			    }
-			    catch (MessageQueue.ShutdownException e) {
-				fail("Unexpected ShutdownException");
-			    }
+  public void testManyQueuersAndDequeuers() throws Exception {
 
-			    try {
-				Thread.sleep(random.nextInt(40));
-			    }
-			    catch (InterruptedException e) {
-			    }
-			}
-		    }
-		};
+    final Thread[] queuers = new Thread[6];
+    final Thread[] dequeuers = new Thread[3];
+    final Random random = new Random();
 
-	    queuers[i].start();
-	}
+    for (int i=0; i<queuers.length; ++i) {
+      queuers[i] = new Thread() {
+          public void run() {
+            for (int j=0; j<10; ++j) {
+              try {
+                m_queue.queue(new SimpleMessage(0));
+              }
+              catch (ThreadSafeQueue.ShutdownException e) {
+                fail("Unexpected ShutdownException");
+              }
 
-	for (int i=0; i<dequeuers.length; ++i) {
-	    dequeuers[i] = new DequeuerThread(20);
-	    dequeuers[i].start();
-	}
+              try {
+                Thread.sleep(random.nextInt(10));
+              }
+              catch (InterruptedException e) {
+              }
+            }
+          }
+        };
 
-	for (int i=0; i<dequeuers.length; ++i) {
-	    dequeuers[i].join();
-	}
-
-	assertNull(m_queue.dequeue(false));
+      queuers[i].start();
     }
 
-    public void testExceptionPropagation() throws Exception
-    {
-	// m_queue does not allow exceptions to be queued.
-	try {
-	    m_queue.queue(new CommunicationException(""));
-	    fail("Expected a CommunicationException");
-	}
-	catch (RuntimeException e) {
-	}
+    for (int i=0; i<dequeuers.length; ++i) {
+      dequeuers[i] = new DequeuerThread(20);
+      dequeuers[i].start();
+    }
 
-	final MessageQueue exceptionQueue = new MessageQueue(true);
+    for (int i=0; i<dequeuers.length; ++i) {
+      dequeuers[i].join();
+    }
 
-	final CommunicationException[] exceptions = {
-	    new CommunicationException("Exception 1"),
-	    new CommunicationException("Exception 2"),
-	};
+    assertNull(m_queue.dequeue(false));
+  }
 
-	final Message[] messages = {
-	    new SimpleMessage(0),
-	    new SimpleMessage(999),
-	};
+  public void testExceptionPropagation() throws Exception {
 
-	for (int i=0; i<messages.length; ++i) {
-	    exceptionQueue.queue(exceptions[i]);
-	    exceptionQueue.queue(messages[i]);
-	}
+    // m_queue does not allow exceptions to be queued.
+    try {
+      m_queue.queue(new CommunicationException(""));
+      fail("Expected a RuntimeException");
+    }
+    catch (RuntimeException e) {
+    }
 
-	for (int i=0; i<messages.length; ++i) {
-	    try {
-		exceptionQueue.dequeue(false);
-		fail("Expected a CommunicationException");
-	    }
-	    catch (CommunicationException e) {
-		assertSame(exceptions[i], e.getNestedThrowable());
-	    }
+    final MessageQueue exceptionQueue = new MessageQueue(true);
 
-	    assertSame(messages[i], exceptionQueue.dequeue(false));
-	}
+    final CommunicationException[] exceptions = {
+      new CommunicationException("Exception 1"),
+      new CommunicationException("Exception 2"),
+    };
+
+    final Message[] messages = {
+      new SimpleMessage(0),
+      new SimpleMessage(999),
+    };
+
+    for (int i=0; i<messages.length; ++i) {
+      exceptionQueue.queue(exceptions[i]);
+      exceptionQueue.queue(messages[i]);
+    }
+
+    for (int i=0; i<messages.length; ++i) {
+      try {
+        exceptionQueue.dequeue(false);
+        fail("Expected a CommunicationException");
+      }
+      catch (CommunicationException e) {
+        assertSame(exceptions[i], e.getNestedThrowable());
+      }
+
+      assertSame(messages[i], exceptionQueue.dequeue(false));
+    }
 	
-	assertNull(m_queue.dequeue(false));
-    }
-    
+    assertNull(m_queue.dequeue(false));
+  }
 
-    private class DequeuerThread extends Thread
-    {
-	private List m_messages = new LinkedList();
-	private Exception m_exception;
-	private int m_howMany;
+  private class DequeuerThread extends Thread {
 
-	public DequeuerThread(int howMany) 
-	{
-	    m_howMany = howMany;
-	}
+    private List m_messages = new LinkedList();
+    private Exception m_exception;
+    private int m_howMany;
 
-	public List getMessages()
-	{
-	    return m_messages;
-	}
-
-	public Exception getException()
-	{
-	    return m_exception;
-	}
-
-	public void run()
-	{
-	    m_exception = null;
-
-	    try {
-		while (m_howMany-- > 0) {
-		    m_messages.add(m_queue.dequeue(true));
-		}
-	    }
-	    catch (Exception e) {
-		m_exception = e;
-	    }
-	}
+    public DequeuerThread(int howMany) {
+      m_howMany = howMany;
     }
 
-    private  static class SimpleMessage extends Message
-    {
-	private static Random s_random = new Random();
-
-	private final String m_text = "Some message";
-	private final int m_random = s_random.nextInt();
-	private final int[] m_padding;
-
-	public SimpleMessage(int paddingSize)
-	{
-	    m_padding = new int[paddingSize];
-
-	    for (int i=0; i<paddingSize; i++) {
-		m_padding[i] = i;
-	    }
-	}
-
-	public String toString()
-	{
-	    return "(" + m_text + ", " + m_random + ")";
-	}
-
-	public boolean payloadEquals(Message o) 
-	{
-	    if (o == this) {
-		return true;
-	    }
-
-	    if (!(o instanceof SimpleMessage)) {
-		return false;
-	    }
-
-	    final SimpleMessage other = (SimpleMessage)o;
-
-	    return
-		m_text.equals(other.m_text) &&
-		m_random == other.m_random;
-	}
+    public List getMessages() {
+      return m_messages;
     }
+
+    public Exception getException() {
+      return m_exception;
+    }
+
+    public void run() {
+
+      m_exception = null;
+
+      try {
+        while (m_howMany-- > 0) {
+          m_messages.add(m_queue.dequeue(true));
+        }
+      }
+      catch (Exception e) {
+        m_exception = e;
+      }
+    }
+  }
 }
