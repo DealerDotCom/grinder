d96a049ec3f5fac04a31859f02e4b108052a30da
Philip Aston <philipa@mail.com>
2003-12-21 13:42:10 +0000

    Fix comment.

--- tests-src/net/grinder/communication/TestAcceptor.java
+++ tests-src/net/grinder/communication/TestAcceptor.java
@@ -1,4 +1,4 @@
-// Copyright (C) 2000, 2001, 2002, 2003 Philip Aston
+// Copyright (C) 2003 Philip Aston
 // All rights reserved.
 //
 // This file is part of The Grinder software distribution. Refer to
