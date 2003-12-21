1a72688f1d4bb310c347f647a673e2f47c355392
Philip Aston <philipa@mail.com>
2003-12-21 18:22:32 +0000

    Kill extra imports.

--- tests-src/net/grinder/communication/TestQueuedSenderDecorator.java
+++ tests-src/net/grinder/communication/TestQueuedSenderDecorator.java
@@ -21,14 +21,7 @@
 
 package net.grinder.communication;
 
-import java.io.InputStream;
-import java.io.ObjectInputStream;
-import java.io.StreamCorruptedException;
-import java.net.InetAddress;
-import java.net.ServerSocket;
-import java.net.Socket;
 import java.util.ArrayList;
-import java.util.Iterator;
 import java.util.List;
 
 import junit.framework.TestCase;
