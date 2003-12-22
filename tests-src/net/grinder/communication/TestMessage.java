23a596fbdaedb50fae6e243add8fe4d87464da0b
Philip Aston <philipa@mail.com>
2003-12-22 18:51:53 +0000

    Test concrete messages.

--- tests-src/net/grinder/communication/TestMessage.java
+++ tests-src/net/grinder/communication/TestMessage.java
@@ -22,13 +22,18 @@
 package net.grinder.communication;
 
 import junit.framework.TestCase;
-import junit.swingui.TestRunner;
-//import junit.textui.TestRunner;
 
 import java.io.ByteArrayInputStream;
 import java.io.ByteArrayOutputStream;
 import java.io.ObjectInputStream;
 import java.io.ObjectOutputStream;
+import java.util.Collection;
+import java.util.HashSet;
+import java.util.Random;
+
+import net.grinder.statistics.ExpressionView;
+import net.grinder.statistics.StatisticsView;
+import net.grinder.statistics.TestStatisticsMap;
 
 
 /**
@@ -38,6 +43,8 @@ import java.io.ObjectOutputStream;
  * @version $Revision$
  */
 public class TestMessage extends TestCase {
+
+  private static Random s_random = new Random();
 
   public TestMessage(String name) {
     super(name);
@@ -74,14 +81,10 @@ public class TestMessage extends TestCase {
     assertEquals(12345l, message.getSequenceNumber());
   }
 
-  public void testSerialisation() throws Exception {
-    long sequenceNumber = Integer.MAX_VALUE;
+  private Message serialise(Message original) throws Exception {
 
-    final Message original0 = new MyMessage();
-    original0.setSenderInformation("grinderID", "uniqueID", sequenceNumber++);
-
-    final Message original1 = new MyMessage();
-    original1.setSenderInformation("grinderID", "uniqueID", sequenceNumber++);
+    original.setSenderInformation("grinderID", "uniqueID",
+                                  s_random.nextLong());
 
     final ByteArrayOutputStream byteOutputStream =
       new ByteArrayOutputStream();
@@ -89,20 +92,22 @@ public class TestMessage extends TestCase {
     final ObjectOutputStream objectOutputStream =
       new ObjectOutputStream(byteOutputStream);
 
-    objectOutputStream.writeObject(original0);
-    objectOutputStream.writeObject(original1);
-
+    objectOutputStream.writeObject(original);
     objectOutputStream.close();
 
     final ObjectInputStream objectInputStream =
       new ObjectInputStream(
         new ByteArrayInputStream(byteOutputStream.toByteArray()));
 
-    final Message received0 = (Message)objectInputStream.readObject();
-    final Message received1 = (Message)objectInputStream.readObject();
+    final Message received = (Message)objectInputStream.readObject();
+    assertTrue(received != original);
+    assertEquals(original, received);
 
-    assertEquals(original0, received0);
-    assertEquals(original1, received1);
+    return received;
+  }
+
+  public void testSerialisation() throws Exception {
+    serialise(new MyMessage());
   }
 
   public void testEquals() throws Exception {
@@ -144,5 +149,97 @@ public class TestMessage extends TestCase {
   }
 
   private static class MyMessage extends Message {
+  }
+
+  public void testCloseCommunicationMessage() throws Exception {
+    final Message original = new CloseCommunicationMessage();
+    final Message received = (CloseCommunicationMessage) serialise(original);
+
+    assertEquals(original, received);
+  }
+
+  public void testRegisterStatisticsViewMessage() throws Exception {
+
+    final StatisticsView statisticsView = new StatisticsView();
+    statisticsView.add(new ExpressionView("One", "blah", "userLong0"));
+
+    final RegisterStatisticsViewMessage original =
+      new RegisterStatisticsViewMessage(statisticsView);
+
+    assertEquals(1, original.getStatisticsView().getExpressionViews().length);
+
+    final RegisterStatisticsViewMessage received =
+      (RegisterStatisticsViewMessage) serialise(original);
+
+    assertEquals(1, received.getStatisticsView().getExpressionViews().length);
+    assertEquals(original.getStatisticsView().getExpressionViews()[0],
+                 received.getStatisticsView().getExpressionViews()[0]);
+  }
+
+  public void testRegisterTestsMessage() throws Exception {
+
+    final Collection c = new HashSet();
+
+    final RegisterTestsMessage original = new RegisterTestsMessage(c);
+
+    assertEquals(c, original.getTests());
+
+    final RegisterTestsMessage received =
+      (RegisterTestsMessage) serialise(original);
+
+    assertEquals(original.getTests(), received.getTests());
+  }
+
+  public void testReportStatisticsMessage() throws Exception {
+
+    final TestStatisticsMap statisticsDelta = new TestStatisticsMap();
+
+    final ReportStatisticsMessage original =
+      new ReportStatisticsMessage(statisticsDelta);
+
+    assertEquals(statisticsDelta, original.getStatisticsDelta());
+
+    final ReportStatisticsMessage received =
+      (ReportStatisticsMessage) serialise(original);
+
+    assertEquals(original.getStatisticsDelta(), received.getStatisticsDelta());
+  }
+
+  public void testReportStatusMessage() throws Exception {
+
+    final ReportStatusMessage original =
+      new ReportStatusMessage((short)1, (short)2, (short)3);
+
+    assertEquals(1, original.getState());
+    assertEquals(2, original.getNumberOfRunningThreads());
+    assertEquals(3, original.getTotalNumberOfThreads());
+
+    final ReportStatusMessage received =
+      (ReportStatusMessage) serialise(original);
+
+    assertEquals(1, received.getState());
+    assertEquals(2, received.getNumberOfRunningThreads());
+    assertEquals(3, received.getTotalNumberOfThreads());
+  }
+
+  public void testResetGrinderMessage() throws Exception {
+    final Message original = new ResetGrinderMessage();
+    final Message received = (ResetGrinderMessage) serialise(original);
+
+    assertEquals(original, received);
+  }
+
+  public void testStartGrinderMessage() throws Exception {
+    final Message original = new StartGrinderMessage();
+    final Message received = (StartGrinderMessage) serialise(original);
+
+    assertEquals(original, received);
+  }
+
+  public void testStopGrinderMessage() throws Exception {
+    final Message original = new StopGrinderMessage();
+    final Message received = (StopGrinderMessage) serialise(original);
+
+    assertEquals(original, received);
   }
 }
