8e52742162e0f48b9c5474872ad1445c4007c0d4
Philip Aston <philipa@mail.com>
2003-12-30 16:47:00 +0000

    ErrorDialogHandler now assumes that exceptions other than DisplayMessageConsoleExceptions are unexpected.

--- src/net/grinder/console/swingui/ScriptFilesPanel.java
+++ src/net/grinder/console/swingui/ScriptFilesPanel.java
@@ -89,8 +89,7 @@ final class ScriptFilesPanel extends JPanel {
             }
           }
           catch (Exception e) {
-            new ErrorDialogHandler(frame, resources).handleException(
-              e, resources.getString("unexpectedError.title"));
+            new ErrorDialogHandler(frame, resources).handleException(e);
           }
         }
       }
