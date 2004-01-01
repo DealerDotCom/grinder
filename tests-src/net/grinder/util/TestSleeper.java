fb4d932ced77c09ac893504bd84a3048363472dd
Philip Aston <philipa@mail.com>
2004-01-01 12:25:46 +0000

    Tweak timings.

--- tests-src/net/grinder/util/TestSleeper.java
+++ tests-src/net/grinder/util/TestSleeper.java
@@ -1,4 +1,4 @@
-// Copyright (C) 2001, 2002 Philip Aston
+// Copyright (C) 2001, 2002, 2003, 2004 Philip Aston
 // All rights reserved.
 //
 // This file is part of The Grinder software distribution. Refer to
@@ -22,8 +22,6 @@
 package net.grinder.util;
 
 import junit.framework.TestCase;
-import junit.swingui.TestRunner;
-//import junit.textui.TestRunner;
 
 
 /**
@@ -32,190 +30,166 @@ import junit.swingui.TestRunner;
  * @author Philip Aston
  * @version $Revision$
  */
-public class TestSleeper extends TestCase
-{
-    public static void main(String[] args)
-    {
-	TestRunner.run(TestSleeper.class);
+public class TestSleeper extends TestCase {
+
+  public TestSleeper(String name) {
+    super(name);
+  }
+
+  public void testConstruction() throws Exception {
+    try {
+      new Sleeper(-1, 1, null);
+      fail("IllegalArgumentException expected");
+    }
+    catch (IllegalArgumentException e) {
     }
 
-    public TestSleeper(String name)
-    {
-	super(name);
+    try {
+      new Sleeper(1, -1, null);
+      fail("IllegalArgumentException expected");
+    }
+    catch (IllegalArgumentException e) {
     }
 
-    public void testConstruction() throws Exception
-    {
-	try {
-	    new Sleeper(-1, 1, null);
-	    fail("IllegalArgumentException expected");
-	}
-	catch (IllegalArgumentException e) {
-	}
+    new Sleeper(1, 1, null);
+  }
 
-	try {
-	    new Sleeper(1, -1, null);
-	    fail("IllegalArgumentException expected");
-	}
-	catch (IllegalArgumentException e) {
-	}
+  public void testSleepNormal() throws Exception {
+    // Warm up hotspot.
+    final Sleeper sleep0 = new Sleeper(1, 0, null);
 
-	new Sleeper(1, 1, null);
+    Time time0 = new Time(0, 1000) {
+        void doIt() throws Exception { sleep0.sleepNormal(10); }
+      };
+
+    for (int i=0; i<10; i++) { time0.run(); }
+
+    // Now do the tests.
+    final Sleeper sleep1 = new Sleeper(1, 0, null);
+
+    assertTrue(
+      new Time(50, 70) {
+        void doIt() throws Exception  { sleep1.sleepNormal(50); }
+      }.run());
+
+    final Sleeper sleep2 = new Sleeper(2, 0, null);
+
+    assertTrue(
+      new Time(100, 120) {
+        void doIt() throws Exception  { sleep2.sleepNormal(50); }
+      }.run());
+
+    final Sleeper sleep3 = new Sleeper(1, 0.1, null);
+
+    final Time time = new Time(40, 60) {
+        void doIt() throws Exception { sleep3.sleepNormal(50);}
+      };
+
+    int in = 0;
+    for (int i=0; i<30; i++) {
+      if (time.run()) {
+        ++in;
+      }
     }
 
-    public void testSleepNormal() throws Exception
-    {
-	// Warm up hotspot.
-	final Sleeper sleep0 = new Sleeper(1, 0, null);
+    assertTrue(in > 20);
+  }
 
-	Time time0 = new Time(0, 1000)
-	{
-	    void doIt() throws Exception { sleep0.sleepNormal(10); }
-	};
+  public void testSleepFlat() throws Exception {
+    // Warm up hotspot.
+    final Sleeper sleep0 = new Sleeper(1, 0, null);
 
-	for (int i=0; i<10; i++) { time0.run(); }
+    Time time0 = new Time(0, 1000)
+      {
+        void doIt() throws Exception { sleep0.sleepFlat(10); }
+      };
 
-	// Now do the tests.
-	final Sleeper sleep1 = new Sleeper(1, 0, null);
+    for (int i=0; i<10; i++) { time0.run(); }
 
-	assertTrue(
-	    new Time(50, 70) {
-		void doIt() throws Exception  { sleep1.sleepNormal(50); }
-	    }.run());
+    // Now do the tests.
+    final Sleeper sleep1 = new Sleeper(1, 0, null);
 
-	final Sleeper sleep2 = new Sleeper(2, 0, null);
+    assertTrue(
+      new Time(0, 70) {
+        void doIt() throws Exception  { sleep1.sleepFlat(50); }
+      }.run());
 
-	assertTrue(
-	    new Time(100, 120) {
-		void doIt() throws Exception  { sleep2.sleepNormal(50); }
-	    }.run());
+    final Sleeper sleep2 = new Sleeper(2, 0, null);
 
-	final Sleeper sleep3 = new Sleeper(1, 0.1, null);
+    assertTrue(
+      new Time(0, 120) {
+        void doIt() throws Exception  { sleep2.sleepFlat(50); }
+      }.run());
+  }
 
-	final Time time = new Time(90, 110) {
-		void doIt() throws Exception { sleep3.sleepNormal(100);}
-	    };
+  public void testShutdown() throws Exception {
+    final TakeFifty t1 = new TakeFifty();
 
-	int in = 0;
-	for (int i=0; i<30; i++) {
-	    if (time.run()) {
-		++in;
-	    }
-	}
+    assertTrue(
+      new Time(500, 1000) {
+        void doIt() throws Exception {
+          t1.start();
+          Thread.sleep(500);
+          t1.getSleeper().shutdown();
+          t1.join();
+        }
+      }.run());
+  }
 
-	assertTrue(in > 20);
+  public void testShutdownAllCurrentSleepers() throws Exception {
+    final Thread t1 = new TakeFifty();
+    final Thread t2 = new TakeFifty();
+
+    assertTrue(
+      new Time(500, 1000) {
+        void doIt() throws Exception {
+          t1.start();
+          t2.start();
+          Thread.sleep(500);
+          Sleeper.shutdownAllCurrentSleepers();
+          t1.join();
+          t2.join();
+        }
+      }.run());
+  }
+
+  private final class TakeFifty extends Thread {
+    private final Sleeper m_sleeper;
+
+    public TakeFifty() throws Sleeper.ShutdownException {
+      m_sleeper = new Sleeper(1, 0, null);
     }
 
-    public void testSleepFlat() throws Exception
-    {
-	// Warm up hotspot.
-	final Sleeper sleep0 = new Sleeper(1, 0, null);
-
-	Time time0 = new Time(0, 1000)
-	{
-	    void doIt() throws Exception { sleep0.sleepFlat(10); }
-	};
-
-	for (int i=0; i<10; i++) { time0.run(); }
-
-	// Now do the tests.
-	final Sleeper sleep1 = new Sleeper(1, 0, null);
-
-	assertTrue(
-	    new Time(0, 70) {
-		void doIt() throws Exception  { sleep1.sleepFlat(50); }
-	    }.run());
-
-	final Sleeper sleep2 = new Sleeper(2, 0, null);
-
-	assertTrue(
-	    new Time(0, 120) {
-		void doIt() throws Exception  { sleep2.sleepFlat(50); }
-	    }.run());
+    public final void run() {
+      try {
+        m_sleeper.sleepNormal(50000);
+      }
+      catch (Sleeper.ShutdownException e) {
+      }
     }
 
-    public void testShutdown() throws Exception
-    {
-	final TakeFifty t1 = new TakeFifty();
+    public final Sleeper getSleeper() {
+      return m_sleeper;
+    }
+  }
 
-	assertTrue(
-	    new Time(1000, 1200)
-	    {
-		void doIt() throws Exception
-		{
-		    t1.start();
-		    Thread.sleep(1000);
-		    t1.getSleeper().shutdown();
-		    t1.join();
-		}
-	    }.run());
+  private abstract class Time {
+    private final double m_expectedMin;
+    private final double m_expectedMax;
+
+    public Time(double expectedMin, double expectedMax) {
+      m_expectedMin = expectedMin;
+      m_expectedMax = expectedMax; // A bit of leeway.
     }
 
-    public void testShutdownAllCurrentSleepers() throws Exception
-    {
-	final Thread t1 = new TakeFifty();
-	final Thread t2 = new TakeFifty();
+    abstract void doIt() throws Exception;
 
-	assertTrue(
-	    new Time(1000, 1200)
-	    {
-		void doIt() throws Exception
-		{
-		    t1.start();
-		    t2.start();
-		    Thread.sleep(1000);
-		    Sleeper.shutdownAllCurrentSleepers();
-		    t1.join();
-		    t2.join();
-		}
-	    }.run());
+    public boolean run() throws Exception {
+      final long then = System.currentTimeMillis();
+      doIt();
+      final long time = System.currentTimeMillis() - then;
+
+      return m_expectedMin <= time && m_expectedMax >= time;
     }
-
-    private final class TakeFifty extends Thread
-    {
-	private final Sleeper m_sleeper;
-
-	public TakeFifty() throws Sleeper.ShutdownException
-	{
-	    m_sleeper = new Sleeper(1, 0, null);
-	}
-
-	public final void run()
-	{
-	    try {
-		m_sleeper.sleepNormal(50000);
-	    }
-	    catch (Sleeper.ShutdownException e) {
-	    }
-	}
-
-	public final Sleeper getSleeper()
-	{
-	    return m_sleeper;
-	}
-    }
-
-    private abstract class Time
-    {
-	private final double m_expectedMin;
-	private final double m_expectedMax;
-
-	public Time(double expectedMin, double expectedMax)
-	{
-	    m_expectedMin = expectedMin;
-	    m_expectedMax = expectedMax; // A bit of leeway.
-	}
-
-	abstract void doIt() throws Exception;
-
-	public boolean run() throws Exception
-	{
-	    final long then = System.currentTimeMillis();
-	    doIt();
-	    final long time = System.currentTimeMillis() - then;
-
-	    return m_expectedMin <= time && m_expectedMax >= time;
-	}
-    }
-
+  }
 }
