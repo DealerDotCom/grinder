49669f31abe8846bccff4c09048b033c836bde4c
Philip Aston <philipa@mail.com>
2004-03-06 11:34:49 +0000

    JUnit 3.8.1, Checkstyle 3.3.

--- etc/buildenv.sh
+++ etc/buildenv.sh
@@ -2,11 +2,10 @@ export ANT_HOME=/opt/ant/jakarta-ant-1.5.1
 export JAVA_HOME=/opt/jdk1.3.1_02/
 export PATH=${ANT_HOME}/bin:${JAVA_HOME}/bin:$PATH
 
-#CHECKSTYLE=/opt/checkstyle/checkstyle-3.2/checkstyle-all-3.2.jar:/opt/checkstyle/checkstyle-3.2/checkstyle-3.2-optional.jar
 JAKARTA_REGEXP=/opt/jakarta-regexp/jakarta-regexp-1.2/jakarta-regexp-1.2.jar
 JSSE_HOME=/opt/jsse/jsse1.0.2/lib/
 JSSE=${JSSE_HOME}/jsse.jar:${JSSE_HOME}/jnet.jar:${JSSE_HOME}/jcert.jar
-JUNIT=/opt/junit/junit3.7/junit.jar
+JUNIT=/opt/junit/junit3.8.1/junit.jar
 XALAN=/opt/xalan/xalan-j_2_3_1/bin/xalan.jar:/opt/xalan/xalan-j_2_3_1/bin/xml-apis.jar
 
 export CLASSPATH=$(cygpath -w -p "${JUNIT}:${JSSE}:${XALAN}")
