377985688246a26d7f261c1b38cd550d441a9f77
Philip Aston <philipa@mail.com>
2004-03-21 11:10:46 +0000

    Correct JavaDoc.

--- src/net/grinder/communication/Receiver.java
+++ src/net/grinder/communication/Receiver.java
@@ -1,4 +1,4 @@
-// Copyright (C) 2000, 2001, 2002, 2003 Philip Aston
+// Copyright (C) 2000, 2001, 2002, 2003, 2004 Philip Aston
 // All rights reserved.
 //
 // This file is part of The Grinder software distribution. Refer to
@@ -39,7 +39,7 @@ public interface Receiver {
    *
    * @return The message or <code>null</code> if shut down.
    * @throws CommunicationException If an IO exception occurs
-   * reading the mesage.
+   * reading the message.
    */
   Message waitForMessage() throws CommunicationException;
 
