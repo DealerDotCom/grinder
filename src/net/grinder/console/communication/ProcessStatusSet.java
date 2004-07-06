efcfcde639d7fcb18d687a5684288b0f55979249
Philip Aston <philipa@mail.com>
2004-07-06 19:01:22 +0000

    Checkstyle.

--- src/net/grinder/console/communication/ProcessStatusSet.java
+++ src/net/grinder/console/communication/ProcessStatusSet.java
@@ -21,17 +21,6 @@
 
 package net.grinder.console.communication;
 
-import java.util.Arrays;
-import java.util.Comparator;
-import java.util.HashMap;
-import java.util.HashSet;
-import java.util.Iterator;
-import java.util.LinkedList;
-import java.util.List;
-import java.util.Map;
-import java.util.Set;
-import java.util.Timer;
-import java.util.TimerTask;
 
 import net.grinder.common.ProcessStatus;
 
