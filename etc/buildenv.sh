6f51f70aa928f1941878de35e3241354094ffd1c
Philip Aston <philipa@mail.com>
2004-08-24 10:17:48 +0000

    Glass Jar out, Clover in.

--- etc/buildenv.sh
+++ etc/buildenv.sh
@@ -6,6 +6,8 @@ JAKARTA_REGEXP=/opt/jakarta-regexp/jakarta-regexp-1.2/jakarta-regexp-1.2.jar
 JSSE_HOME=/opt/jsse/jsse1.0.2/lib/
 JSSE=${JSSE_HOME}/jsse.jar:${JSSE_HOME}/jnet.jar:${JSSE_HOME}/jcert.jar
 JUNIT=/opt/junit/junit3.8.1/junit.jar
+# Clover home also needs to be set in localpaths.properties.
+CLOVER=/opt/clover/clover-1.3_02/lib/clover.jar
 XALAN=/opt/xalan/xalan-j_2_3_1/bin/xalan.jar:/opt/xalan/xalan-j_2_3_1/bin/xml-apis.jar
 
-export CLASSPATH=$(cygpath -w -p "${JUNIT}:${JSSE}:${XALAN}")
+export CLASSPATH=$(cygpath -w -p "${JUNIT}:${CLOVER}:${JSSE}:${XALAN}")
