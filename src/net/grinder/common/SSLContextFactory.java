cca4869508ad48c35a489c742b58714ee091a10d
Philip Aston <philipa@mail.com>
2004-10-10 15:44:03 +0000

    Common interface for SSLContext factories.

---
+++ src/net/grinder/common/SSLContextFactory.java
@@ -0,0 +1,76 @@
+// Copyright (C) 2004 Philip Aston
+// All rights reserved.
+//
+// This file is part of The Grinder software distribution. Refer to
+// the file LICENSE which is part of The Grinder distribution for
+// licensing details. The Grinder distribution is available on the
+// Internet at http://grinder.sourceforge.net/
+//
+// THIS SOFTWARE IS PROVIDED BY THE COPYRIGHT HOLDERS AND CONTRIBUTORS
+// "AS IS" AND ANY EXPRESS OR IMPLIED WARRANTIES, INCLUDING, BUT NOT
+// LIMITED TO, THE IMPLIED WARRANTIES OF MERCHANTABILITY AND FITNESS
+// FOR A PARTICULAR PURPOSE ARE DISCLAIMED. IN NO EVENT SHALL THE
+// REGENTS OR CONTRIBUTORS BE LIABLE FOR ANY DIRECT, INDIRECT,
+// INCIDENTAL, SPECIAL, EXEMPLARY, OR CONSEQUENTIAL DAMAGES
+// (INCLUDING, BUT NOT LIMITED TO, PROCUREMENT OF SUBSTITUTE GOODS OR
+// SERVICES; LOSS OF USE, DATA, OR PROFITS; OR BUSINESS INTERRUPTION)
+// HOWEVER CAUSED AND ON ANY THEORY OF LIABILITY, WHETHER IN CONTRACT,
+// STRICT LIABILITY, OR TORT (INCLUDING NEGLIGENCE OR OTHERWISE)
+// ARISING IN ANY WAY OUT OF THE USE OF THIS SOFTWARE, EVEN IF ADVISED
+// OF THE POSSIBILITY OF SUCH DAMAGE.
+
+package net.grinder.common;
+
+// Use old sun package for J2SE 1.3/JSSE 1.0.2 compatibility.
+import com.sun.net.ssl.SSLContext;
+
+
+/**
+ * Factory for SSLContext's.
+ *
+ * @author Philip Aston
+ * @version $Revision$
+ */
+public interface SSLContextFactory {
+
+  /**
+   * Get a JSSE {@link SSLContext}. This can be used to obtain an
+   * {@link javax.net.ssl.SSLSocketFactory}.
+   *
+   * <p><b>Do not use The Grinder to implement any SSL communication that
+   * you want to be secure.</b></p>
+   *
+   * @return The SSL context.
+   * @exception SSLContextFactoryException If the SSLContext could not
+   * be found/created.
+   * @see net.grinder.script.SSLControl
+   */
+  SSLContext getSSLContext() throws SSLContextFactoryException;
+
+  /**
+   * Exception that indicates problem creating an SSLContext.
+   */
+  public static final class SSLContextFactoryException
+    extends GrinderException {
+
+    /**
+     * Constructor.
+     *
+     * @param message Helpful message.
+     */
+    public SSLContextFactoryException(String message) {
+      super(message);
+    }
+
+    /**
+     * Constructor.
+     *
+     * @param message Helpful message.
+     * @param t A nested <code>Throwable</code>
+     */
+    public SSLContextFactoryException(String message, Throwable t) {
+      super(message, t);
+    }
+  }
+}
+
