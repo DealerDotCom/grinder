9239b19e64ce9da1e40da61eec8c17d0575092b1
Philip Aston <philipa@mail.com>
2005-03-08 08:09:12 +0000

    Odds and ends.

---
+++ etc/setenv.cmd
@@ -0,0 +1,9 @@
+set GRINDER_HOME=d:\src\grinder3
+set JUNIT=d:\opt\junit\junit3.8.1\junit.jar
+set JAVA_HOME=d:\opt\jdk1.3.1_02\
+set ANT_HOME=d:\opt\ant\jakarta-ant-1.5.1
+
+set CLASSPATH=%GRINDER_HOME%\build\classes;%GRINDER_HOME%\lib\jakarta-oro-2.0.6.jar;%GRINDER_HOME%\lib\jython.jar;%JUNIT%
+
+set PATH=%ANT_HOME%\bin;%JAVA_HOME%\bin;%PATH%
+
