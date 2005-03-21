d6a4fdcbe9e19a19bd942b4c33d2ca0310d25ca8
Philip Aston <philipa@mail.com>
2005-03-21 08:05:30 +0000

    Exercise standard deviation.

--- examples/console.py
+++ examples/console.py
@@ -4,23 +4,23 @@
 from net.grinder.script.Grinder import grinder
 from net.grinder.script import Test
 from java.util import Random
+from java.lang import Math
 
 r = Random()
 
-def doIt():
-    grinder.sleep(r.nextInt(100))
+def doIt(v):
+    t = 500 + r.nextGaussian() * v * 10
+    grinder.sleep(int(t), 0)
     pass
 
-tests = [ Test(i, "Test %s" % i).wrap(doIt) for i in range(0, 100) ]
+tests = [ Test(i, "Test %s" % i).wrap(doIt) for i in range(0, 10) ]
 
 class TestRunner:
     def __call__(self):
         statistics = grinder.statistics
-        statistics.delayReports = 1
-        
-        for test in tests:
-            test()
-            if r.nextInt() % 500 == 0:
-                statistics.success = 0
+#        statistics.delayReports = 1
 
-        grinder.sleep(100)
+        for test in tests:
+            test(test.__test__.number)
+
+
