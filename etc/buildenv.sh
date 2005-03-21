41ef2a45322412007297c5fac54012e2af72e715
Philip Aston <philipa@mail.com>
2005-03-21 07:55:12 +0000

    Latest Clover.

--- etc/buildenv.sh
+++ etc/buildenv.sh
@@ -7,7 +7,7 @@ JSSE_HOME=/opt/jsse/jsse1.0.2/lib/
 JSSE=${JSSE_HOME}/jsse.jar:${JSSE_HOME}/jnet.jar:${JSSE_HOME}/jcert.jar
 JUNIT=/opt/junit/junit3.8.1/junit.jar
 # Clover home also needs to be set in localpaths.properties.
-CLOVER=/opt/clover/clover-1.3_02/lib/clover.jar
+CLOVER=/opt/clover/clover-1.3.5/lib/clover.jar
 XALAN=/opt/xalan/xalan-j_2_3_1/bin/xalan.jar:/opt/xalan/xalan-j_2_3_1/bin/xml-apis.jar
 
 # Ant now handles cygpath -w if necessary.
