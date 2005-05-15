74e8da311da04a9c1059bb36feac0a78669149d4
Philip Aston <philipa@mail.com>
2005-05-15 09:09:25 +0000

    Update.

--- etc/setenv.cmd
+++ etc/setenv.cmd
@@ -1,9 +1,13 @@
-set GRINDER_HOME=d:\src\grinder3
-set JUNIT=d:\opt\junit\junit3.8.1\junit.jar
-set JAVA_HOME=d:\opt\jdk1.3.1_02\
-set ANT_HOME=d:\opt\ant\jakarta-ant-1.5.1
-
-set CLASSPATH=%GRINDER_HOME%\build\classes;%GRINDER_HOME%\lib\jython.jar;%JUNIT%
-
+set ANT_HOME=d:\opt\ant\apache-ant-1.6.2
+set JAVA_HOME=c:\bea-8.1.4\jdk142_05
 set PATH=%ANT_HOME%\bin;%JAVA_HOME%\bin;%PATH%
+
+set JUNIT=d:\opt\junit\junit3.8.1\junit.jar
+set CLOVER=d:\opt\clover\clover-1.3.5\lib\clover.jar
+
+set GRINDER_HOME=d:\src\grinder3
+
+set CLASSPATH=%GRINDER_HOME%\build\classes;%GRINDER_HOME%\lib\jython.jar;%JUNIT%;%CLOVER%;
+
+
 
