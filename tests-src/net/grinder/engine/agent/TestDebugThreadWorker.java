5a5ff53a203f455addf6653a75abab1cd57553e2
Philip Aston <philipa@mail.com>
2005-11-28 11:13:08 +0000

    Tests for DebugThreadWorker classes.

---
+++ tests-src/net/grinder/engine/agent/TestDebugThreadWorker.java
@@ -0,0 +1,89 @@
+// Copyright (C) 2005 Philip Aston
+// All rights reserved.
+//
+// This file is part of The Grinder software distribution. Refer to
+// the file LICENSE which is part of The Grinder distribution for
+// licensing details. The Grinder distribution is available on the
+// Internet at http://grinder.sourceforge.net/
+//
+// THIS SOFTWARE IS PROVIDED BY THE COPYRIGHT HOLDERS AND CONTRIBUTORS
+// "AS IS" AND ANY EXPRESS OR IMPLIED WARRANTIES, INCLUDING, BUT NOT
+// LIMITED TO, THE IMPLIED WARRANTIES OF MERCHANTABILITY AND FITNESS
+// FOR A PARTICULAR PURPOSE ARE DISCLAIMED. IN NO EVENT SHALL THE
+// REGENTS OR CONTRIBUTORS BE LIABLE FOR ANY DIRECT, INDIRECT,
+// INCIDENTAL, SPECIAL, EXEMPLARY, OR CONSEQUENTIAL DAMAGES
+// (INCLUDING, BUT NOT LIMITED TO, PROCUREMENT OF SUBSTITUTE GOODS OR
+// SERVICES; LOSS OF USE, DATA, OR PROFITS; OR BUSINESS INTERRUPTION)
+// HOWEVER CAUSED AND ON ANY THEORY OF LIABILITY, WHETHER IN CONTRACT,
+// STRICT LIABILITY, OR TORT (INCLUDING NEGLIGENCE OR OTHERWISE)
+// ARISING IN ANY WAY OUT OF THE USE OF THIS SOFTWARE, EVEN IF ADVISED
+// OF THE POSSIBILITY OF SUCH DAMAGE.
+
+package net.grinder.engine.agent;
+
+import java.io.ByteArrayOutputStream;
+import java.io.OutputStream;
+import java.io.PrintStream;
+import java.io.PrintWriter;
+import java.io.StringWriter;
+
+import net.grinder.common.WorkerIdentity;
+import net.grinder.communication.StreamSender;
+import net.grinder.engine.common.EngineException;
+import junit.framework.TestCase;
+
+/**
+ * Unit tests for <code>DebugThreadWorker</code>.
+ *
+ * @author Philip Aston
+ * @version $Revision$
+ */
+public class TestDebugThreadWorker extends TestCase {
+
+  public void testDebugThreadWorker() throws Exception {
+
+    final WorkerIdentity workerIdentity =
+      new AgentIdentityImplementation("test-x").createWorkerIdentity();
+
+    final Worker worker = new DebugThreadWorker(workerIdentity);
+
+    assertEquals(workerIdentity, worker.getIdentity());
+    assertNotNull(worker.getCommunicationStream());
+
+    final int[] resultHolder = { -1 };
+
+    final Thread waitThread = new Thread() {
+      public void run() {
+        try {
+          resultHolder[0] = worker.waitFor();
+        }
+        catch (EngineException e) {
+        }
+        catch (InterruptedException e) {
+        }
+      }
+    };
+
+    waitThread.start();
+
+    assertEquals(-1, resultHolder[0]);
+    assertTrue(waitThread.isAlive());
+
+    final ByteArrayOutputStream byteStream = new ByteArrayOutputStream();
+    final PrintStream redirectedStderr = new PrintStream(byteStream);
+    final PrintStream oldStderr = System.err;
+
+    try {
+      System.setErr(redirectedStderr);
+      new StreamSender(worker.getCommunicationStream()).shutdown();
+      waitThread.join();
+    }
+    finally {
+      System.setErr(oldStderr);
+    }
+
+    assertEquals(-2, resultHolder[0]);
+    final String output = new String(byteStream.toByteArray());
+    assertTrue(output.indexOf("No control stream from agent") > 0);
+  }
+}
---
+++ tests-src/net/grinder/engine/agent/TestDebugThreadWorkerFactory.java
@@ -0,0 +1,68 @@
+// Copyright (C) 2005 Philip Aston
+// All rights reserved.
+//
+// This file is part of The Grinder software distribution. Refer to
+// the file LICENSE which is part of The Grinder distribution for
+// licensing details. The Grinder distribution is available on the
+// Internet at http://grinder.sourceforge.net/
+//
+// THIS SOFTWARE IS PROVIDED BY THE COPYRIGHT HOLDERS AND CONTRIBUTORS
+// "AS IS" AND ANY EXPRESS OR IMPLIED WARRANTIES, INCLUDING, BUT NOT
+// LIMITED TO, THE IMPLIED WARRANTIES OF MERCHANTABILITY AND FITNESS
+// FOR A PARTICULAR PURPOSE ARE DISCLAIMED. IN NO EVENT SHALL THE
+// REGENTS OR CONTRIBUTORS BE LIABLE FOR ANY DIRECT, INDIRECT,
+// INCIDENTAL, SPECIAL, EXEMPLARY, OR CONSEQUENTIAL DAMAGES
+// (INCLUDING, BUT NOT LIMITED TO, PROCUREMENT OF SUBSTITUTE GOODS OR
+// SERVICES; LOSS OF USE, DATA, OR PROFITS; OR BUSINESS INTERRUPTION)
+// HOWEVER CAUSED AND ON ANY THEORY OF LIABILITY, WHETHER IN CONTRACT,
+// STRICT LIABILITY, OR TORT (INCLUDING NEGLIGENCE OR OTHERWISE)
+// ARISING IN ANY WAY OUT OF THE USE OF THIS SOFTWARE, EVEN IF ADVISED
+// OF THE POSSIBILITY OF SUCH DAMAGE.
+
+package net.grinder.engine.agent;
+
+import java.io.ByteArrayOutputStream;
+import java.io.PrintStream;
+
+import net.grinder.common.GrinderProperties;
+import net.grinder.communication.FanOutStreamSender;
+import junit.framework.TestCase;
+
+
+/**
+ * Unit tests for <code>DebugThreadWorkerFactory</code>.
+ *
+ * @author Philip Aston
+ * @version $Revision$
+ */
+public class TestDebugThreadWorkerFactory extends TestCase {
+
+  public void testFactory() throws Exception {
+
+    final AgentIdentityImplementation agentIdentity =
+      new AgentIdentityImplementation("test");
+
+    final GrinderProperties properties = new GrinderProperties();
+
+    final DebugThreadWorkerFactory factory =
+      new DebugThreadWorkerFactory(agentIdentity,
+                                   new FanOutStreamSender(0),
+                                   false,
+                                   null,
+                                   null,
+                                   properties);
+
+    final ByteArrayOutputStream byteStream = new ByteArrayOutputStream();
+    final PrintStream redirectedStderr = new PrintStream(byteStream);
+    final PrintStream oldStderr = System.err;
+
+    try {
+      System.setErr(redirectedStderr);
+      final Worker worker = factory.create(null, null);
+      worker.destroy();
+    }
+    finally {
+      System.setErr(oldStderr);
+    }
+  }
+}
