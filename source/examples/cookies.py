c7120622eec6080711b2e149b6304e1c7e2a89d7
Philip Aston <philipa@mail.com>
2007-11-05 19:33:43 +0000

    Further cookie munching.

--- source/examples/cookies.py
+++ source/examples/cookies.py
@@ -2,11 +2,18 @@
 #
 # HTTP example which shows how to access HTTP cookies.
 #
-# By default, the HTTPClient library handles Cookie interaction and
-# removes the cookie headers from responses. If you want to access
-# them, one way is to define your own CookiePolicyHandler. This script
-# defines a CookiePolicyHandler that simply logs all cookies that are
-# sent or received.
+# The HTTPClient library handles cookie interaction and removes the
+# cookie headers from responses. If you want to access these cookies,
+# one way is to define your own CookiePolicyHandler. This script defines
+# a CookiePolicyHandler that simply logs all cookies that are sent or
+# received.
+#
+# The script also demonstrates how to query what cookies are cached for
+# the current thread, and how add and remove cookies from the cache.
+#
+# If you really want direct control over the cookie headers, you
+# can disable the automatic cookie handling with:
+#    HTTPPluginControl.getConnectionDefaults().useCookies = 0
 
 from net.grinder.script.Grinder import grinder
 from net.grinder.script import Test
@@ -16,6 +23,7 @@ from java.util import Date
 
 log = grinder.logger.output
 
+# Set up a cookie handler to log all cookies that are sent and received.
 class MyCookiePolicyHandler(CookiePolicyHandler):
     def acceptCookie(self, cookie, request, response):
         log("accept cookie: %s" % cookie)
@@ -33,17 +41,34 @@ request1 = test1.wrap(HTTPRequest())
 
 class TestRunner:
     def __call__(self):
-        result = request1.GET("http://localhost:7001/console")
+        # The cache of cookies for each  worker thread will be reset at
+        # the start of each run.
 
-        # Now let's send a cookie.
+        result = request1.GET("http://localhost:7001/console/?request1")
+
+        # If the first response set any cookies for the domain,
+        # they willl be sent back with this request.
+        result2 = request1.GET("http://localhost:7001/console/?request2")
+
+        # Now let's add a new cookie.
+        threadContext = HTTPPluginControl.getThreadHTTPClientContext()
+
         expiryDate = Date()
-        expiryDate.year = 2008
-        expiryDate.month = 12
-        expiryDate.date = 31
+        expiryDate.year += 10
 
         cookie = Cookie("key", "value","localhost", "/", expiryDate, 0)
 
-        CookieModule.addCookie(cookie,
-                               HTTPPluginControl.getThreadHTTPClientContext())
+        CookieModule.addCookie(cookie, threadContext)
 
-        result = request1.GET("http://localhost:7001/console")
+        result = request1.GET("http://localhost:7001/console/?request3")
+
+        # Get all cookies for the current thread and write them to the log
+        cookies = CookieModule.listAllCookies(threadContext)
+        for c in cookies: log("retrieved cookie: %s" % c)
+
+        # Remove any cookie that isn't ours.
+        for c in cookies:
+            if c != cookie: CookieModule.removeCookie(c, threadContext)
+
+        result = request1.GET("http://localhost:7001/console/?request4")
+
