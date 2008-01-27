7fb0d1f79ddd0c0de6f268d9c3dd5980582b8768
Philip Aston <philipa@mail.com>
2008-01-27 15:42:27 +0000

    Coverage.

--- source/tests-src/net/grinder/console/TestConsoleFoundation.java
+++ source/tests-src/net/grinder/console/TestConsoleFoundation.java
@@ -24,11 +24,17 @@ package net.grinder.console;
 import java.io.File;
 import java.net.InetAddress;
 import java.net.ServerSocket;
+import java.util.Collection;
+import java.util.Collections;
 import java.util.HashMap;
 import java.util.Timer;
+import java.util.TimerTask;
+import java.util.regex.Pattern;
 
 import net.grinder.common.Logger;
 import net.grinder.common.LoggerStubFactory;
+import net.grinder.communication.MessageDispatchRegistry;
+import net.grinder.communication.Sender;
 import net.grinder.console.ConsoleFoundation.UI;
 import net.grinder.console.client.ConsoleConnection;
 import net.grinder.console.client.ConsoleConnectionFactory;
@@ -38,12 +44,24 @@ import net.grinder.console.common.StubResources;
 import net.grinder.console.communication.ConsoleCommunication;
 import net.grinder.console.communication.DistributionControl;
 import net.grinder.console.communication.ProcessControl;
+import net.grinder.console.communication.ProcessStatus;
+import net.grinder.console.communication.ProcessStatus.Listener;
+import net.grinder.console.communication.server.DispatchClientCommands;
 import net.grinder.console.distribution.FileDistribution;
+import net.grinder.console.messages.RegisterExpressionViewMessage;
+import net.grinder.console.messages.RegisterTestsMessage;
+import net.grinder.console.messages.ReportStatisticsMessage;
 import net.grinder.console.model.ConsoleProperties;
 import net.grinder.console.model.Model;
+import net.grinder.statistics.ExpressionView;
 import net.grinder.statistics.StatisticsServices;
 import net.grinder.statistics.StatisticsServicesImplementation;
+import net.grinder.statistics.TestStatisticsMap;
 import net.grinder.testutility.AbstractFileTestCase;
+import net.grinder.testutility.CallData;
+import net.grinder.testutility.RandomStubFactory;
+import net.grinder.testutility.StubTimer;
+import net.grinder.util.Directory;
 
 
 /**
@@ -85,7 +103,7 @@ public class TestConsoleFoundation extends AbstractFileTestCase {
     m_loggerStubFactory.assertNoMoreCalls();
   }
 
-  public void XtestSimpleRun() throws Exception {
+  public void testSimpleRun() throws Exception {
 
     final Timer timer = new Timer(true);
 
@@ -122,6 +140,137 @@ public class TestConsoleFoundation extends AbstractFileTestCase {
     foundation.shutdown();
 
     runConsole.join();
+  }
+
+  public void testWireFileDistribution() throws Exception {
+
+    final RandomStubFactory fileDistributionStubFactory =
+      new RandomStubFactory(FileDistribution.class);
+    final FileDistribution fileDistribution =
+      (FileDistribution)fileDistributionStubFactory.getStub();
+
+    final RandomStubFactory processControlStubFactory =
+      new RandomStubFactory(ProcessControl.class);
+    final ProcessControl processControl =
+      (ProcessControl)processControlStubFactory.getStub();
+
+    final ConsoleProperties consoleProperties =
+      new ConsoleProperties(m_resources, new File(getDirectory(), "props"));
+
+    final StubTimer timer = new StubTimer();
+
+    new ConsoleFoundation.WireFileDistribution(fileDistribution,
+                                               consoleProperties,
+                                               timer,
+                                               processControl);
+
+    fileDistributionStubFactory.assertNoMoreCalls();
+
+    assertEquals(6000, timer.getLastDelay());
+    assertEquals(6000, timer.getLastPeriod());
+
+    final TimerTask scanFileTask = timer.getLastScheduledTimerTask();
+    scanFileTask.run();
+    fileDistributionStubFactory.assertSuccess("scanDistributionFiles");
+    fileDistributionStubFactory.assertNoMoreCalls();
+
+    final CallData addListenerCall =
+      processControlStubFactory.assertSuccess("addProcessStatusListener",
+                                              Listener.class);
+    final Listener listener =
+      (Listener) addListenerCall.getParameters()[0];
+
+    final ProcessStatus.ProcessReports[] reports =
+      new ProcessStatus.ProcessReports[0];
+    listener.update(reports, false);
+    fileDistributionStubFactory.assertNoMoreCalls();
+
+    listener.update(reports, true);
+    fileDistributionStubFactory.assertSuccess("getAgentCacheState");
+    fileDistributionStubFactory.assertNoMoreCalls();
+
+    consoleProperties.setDistributionFileFilterExpression(".*");
+    final CallData setFileFilterCall =
+      fileDistributionStubFactory.assertSuccess("setFileFilterPattern",
+                                                Pattern.class);
+    final Pattern pattern = (Pattern)setFileFilterCall.getParameters()[0];
+    assertEquals(".*", pattern.pattern());
+    fileDistributionStubFactory.assertNoMoreCalls();
+
+    final Directory directory = new Directory(new File(getDirectory(), "foo"));
+    consoleProperties.setAndSaveDistributionDirectory(directory);
+    final CallData setDirectoryCall =
+      fileDistributionStubFactory.assertSuccess("setDirectory",
+                                                Directory.class);
+    assertSame(directory, setDirectoryCall.getParameters()[0]);
+
+    consoleProperties.setConsolePort(999);
+    fileDistributionStubFactory.assertNoMoreCalls();
+  }
+
+  public void testWireMessageDispatch() throws Exception {
+    final RandomStubFactory messageDispatchRegistryStubFactory =
+      new RandomStubFactory(MessageDispatchRegistry.class);
+    final MessageDispatchRegistry messageDispatchRegistry =
+      (MessageDispatchRegistry)messageDispatchRegistryStubFactory.getStub();
+
+    final RandomStubFactory consoleCommunicationStubFactory =
+      new RandomStubFactory(ConsoleCommunication.class);
+    final ConsoleCommunication consoleCommunication =
+      (ConsoleCommunication)consoleCommunicationStubFactory.getStub();
+    consoleCommunicationStubFactory.setResult(
+      "getMessageDispatchRegistry", messageDispatchRegistry);
+
+    final RandomStubFactory modelStubFactory =
+      new RandomStubFactory(Model.class);
+    final Model model = (Model)modelStubFactory.getStub();
+
+    final DispatchClientCommands dispatchClientCommands =
+      new DispatchClientCommands(null, null);
+
+    new ConsoleFoundation.WireMessageDispatch(
+      consoleCommunication, model, dispatchClientCommands);
+
+    consoleCommunicationStubFactory.assertSuccess("getMessageDispatchRegistry");
+    consoleCommunicationStubFactory.assertNoMoreCalls();
+
+    final CallData call1 =
+      messageDispatchRegistryStubFactory.assertSuccess(
+        "set", Class.class, Sender.class);
+    assertEquals(RegisterTestsMessage.class, call1.getParameters()[0]);
+
+    final Sender sender1 = (Sender) call1.getParameters()[1];
+    final Collection tests = Collections.EMPTY_SET;
+    sender1.send(new RegisterTestsMessage(tests));
+    modelStubFactory.assertSuccess("registerTests", tests);
+    modelStubFactory.assertNoMoreCalls();
+
+    final CallData call2 =
+      messageDispatchRegistryStubFactory.assertSuccess(
+        "set", Class.class, Sender.class);
+    assertEquals(ReportStatisticsMessage.class, call2.getParameters()[0]);
+
+    final Sender sender2 = (Sender) call2.getParameters()[1];
+    final TestStatisticsMap delta = new TestStatisticsMap();
+    sender2.send(new ReportStatisticsMessage(delta));
+    modelStubFactory.assertSuccess("addTestReport", delta);
+    modelStubFactory.assertNoMoreCalls();
+
+    final CallData call3 =
+      messageDispatchRegistryStubFactory.assertSuccess(
+        "set", Class.class, Sender.class);
+    assertEquals(RegisterExpressionViewMessage.class, call3.getParameters()[0]);
+
+    final Sender sender3 = (Sender) call3.getParameters()[1];
+    final ExpressionView expressionView =
+      StatisticsServicesImplementation.getInstance()
+      .getStatisticExpressionFactory().createExpressionView(
+        "blah", "userLong0", false);
+    sender3.send(new RegisterExpressionViewMessage(expressionView));
+    modelStubFactory.assertSuccess(
+      "registerStatisticExpression", expressionView);
+    modelStubFactory.assertNoMoreCalls();
+
   }
 
   public static class MyUI implements UI {
