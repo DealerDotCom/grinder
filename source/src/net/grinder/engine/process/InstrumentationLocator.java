9c67af0ed0e3a5a9f1c20457a35f426c5741e0fd
Philip Aston <philipa@mail.com>
2009-10-10 16:18:16 +0000

    Implement InstrumentationLocator.

--- source/src/net/grinder/engine/process/InstrumentationLocator.java
+++ source/src/net/grinder/engine/process/InstrumentationLocator.java
@@ -1,4 +1,4 @@
-// Copyright (C) 2008 Philip Aston
+// Copyright (C) 2009 Philip Aston
 // All rights reserved.
 //
 // This file is part of The Grinder software distribution. Refer to
@@ -21,6 +21,20 @@
 
 package net.grinder.engine.process;
 
+import static extra166y.CustomConcurrentHashMap.IDENTITY;
+import static extra166y.CustomConcurrentHashMap.STRONG;
+
+import java.util.List;
+import java.util.ListIterator;
+import java.util.concurrent.ConcurrentMap;
+import java.util.concurrent.CopyOnWriteArrayList;
+
+import net.grinder.common.UncheckedGrinderException;
+import net.grinder.engine.common.EngineException;
+import net.grinder.engine.process.ScriptEngine.TestInstrumentation;
+import net.grinder.util.Pair;
+import extra166y.CustomConcurrentHashMap;
+
 
 /**
  * Static methods that weaved code uses to dispatch enter and exit calls to the
@@ -29,7 +43,24 @@ package net.grinder.engine.process;
  * @author Philip Aston
  * @version $Revision:$
  */
-public class InstrumentationLocator {
+public final class InstrumentationLocator {
+
+  /** Singleton reference used to represent static method invocations. */
+  private static final Object STATIC = new Object();
+
+  // Location strings are interned, so we use an identity hash map.
+  // The mapping between the references and the instrumentation is stored as
+  // an ordered list of (reference, instrumentation) pairs. Again,
+  // identity equality is used to find the appropriate instrumentations for
+  // a reference. The number of instrumented items for each location will be
+  // low (typically 1),
+  private static final ConcurrentMap<String, InstrumentationList>
+    s_instrumentation =
+      new CustomConcurrentHashMap<String, InstrumentationList>(STRONG,
+                                                               IDENTITY,
+                                                               STRONG,
+                                                               IDENTITY,
+                                                               101);
 
   /**
    * Called when a weaved method is entered.
@@ -38,9 +69,28 @@ public class InstrumentationLocator {
    *          The reference used to call the method.
    * @param locationID
    *          Unique identity generated when the method was instrumented.
+   *          Will be interned.
+   * @throws EngineException
    */
   public static void enter(Object target, String locationID) {
-    System.out.printf("PRE(%s, %s)%n", target, locationID);
+
+    final List<Pair<Object, TestInstrumentation>> instrumentationList =
+      s_instrumentation.get(locationID);
+
+    if (instrumentationList != null) {
+      final Object t = target == null ? STATIC : target;
+
+      try {
+        for (Pair<Object, TestInstrumentation> pair : instrumentationList) {
+          if (pair.getFirst() == t) {
+              pair.getSecond().startTest();
+          }
+        }
+      }
+      catch (EngineException e) {
+        throw new InstrumentationFailureException(e);
+      }
+    }
   }
 
   /**
@@ -50,11 +100,83 @@ public class InstrumentationLocator {
    *          The reference used to call the method.
    * @param locationID
    *          Unique identity generated when the method was instrumented.
+   *          Will be interned.
    * @param success
    *          {@code true} if the exit was a normal return, {code false} if an
    *          exception was thrown.
    */
   public static void exit(Object target, String locationID, boolean success) {
-    System.out.printf("POST(%s, %s, %s)%n", target, locationID, success);
+
+    final List<Pair<Object, TestInstrumentation>> instrumentationList =
+      s_instrumentation.get(locationID);
+
+    if (instrumentationList != null) {
+      final Object t = target == null ? STATIC : target;
+
+      // Iterate over instrumentation in reverse.
+      final ListIterator<Pair<Object, TestInstrumentation>> i =
+        instrumentationList.listIterator(instrumentationList.size());
+
+      try {
+        while (i.hasPrevious()) {
+          final Pair<Object, TestInstrumentation> pair = i.previous();
+
+          if (pair.getFirst() == t) {
+              pair.getSecond().endTest(success);
+          }
+        }
+      }
+      catch (EngineException e) {
+        throw new InstrumentationFailureException(e);
+      }
+    }
   }
+
+  static void register(Object target,
+                       String locationID,
+                       TestInstrumentation instrumentation) {
+    // Most locations will have a single instrumentation. When they don't,
+    // we create and discard a list needlessly. This is fine - we are optimising
+    // the enter/exit methods by using concurrent structures, the
+    // instrumentation registration process can be relatively slow.
+    final InstrumentationList
+      newInstrumentations = new InstrumentationListImplementation();
+
+    final InstrumentationList oldInstrumentations =
+      s_instrumentation.putIfAbsent(locationID.intern(),
+                                    newInstrumentations);
+
+    final InstrumentationList instrumentations =
+      oldInstrumentations != null ? oldInstrumentations : newInstrumentations;
+
+    instrumentations.add(
+      new Pair<Object, TestInstrumentation>(
+        target != null ? target : STATIC, instrumentation));
+  }
+
+  /**
+   * Accessor for the unit tests.
+   *
+   * @return Our internal structure.
+   */
+  static ConcurrentMap<String, InstrumentationList> getInstrumentation() {
+    return s_instrumentation;
+  }
+
+  private static final class InstrumentationFailureException
+    extends UncheckedGrinderException {
+
+    private InstrumentationFailureException(EngineException cause) {
+      super("Instrumentation Failure", cause);
+    }
+  }
+
+  /** Poor man's typedef. */
+  private interface InstrumentationList
+    extends List<Pair<Object, TestInstrumentation>> {};
+
+    /** Poor man's typedef. */
+  private static class InstrumentationListImplementation
+    extends CopyOnWriteArrayList<Pair<Object, TestInstrumentation>>
+    implements InstrumentationList {};
 }
---
+++ source/tests-src/net/grinder/engine/process/TestInstrumentationLocator.java
@@ -0,0 +1,296 @@
+// Copyright (C) 2009 Philip Aston
+// All rights reserved.
+//
+// This file is part of The Grinder software distribution. Refer to
+// the file LICENSE which is part of The Grinder distribution for
+// licensing details. The Grinder distribution is available on the
+// Internet at http://grinder.sourceforge.net/
+//
+// THIS SOFTWARE IS PROVIDED BY THE COPYRIGHT HOLDERS AND CONTRIBUTORS
+// "AS IS" AND ANY EXPRESS OR IMPLIED WARRANTIES, INCLUDING, BUT NOT
+// LIMITED TO, THE IMPLIED WARRANTIES OF MERCHANTABILITY AND FITNESS
+// FOR A PARTICULAR PURPOSE ARE DISCLAIMED. IN NO EVENT SHALL THE
+// COPYRIGHT HOLDERS OR CONTRIBUTORS BE LIABLE FOR ANY DIRECT, INDIRECT,
+// INCIDENTAL, SPECIAL, EXEMPLARY, OR CONSEQUENTIAL DAMAGES
+// (INCLUDING, BUT NOT LIMITED TO, PROCUREMENT OF SUBSTITUTE GOODS OR
+// SERVICES; LOSS OF USE, DATA, OR PROFITS; OR BUSINESS INTERRUPTION)
+// HOWEVER CAUSED AND ON ANY THEORY OF LIABILITY, WHETHER IN CONTRACT,
+// STRICT LIABILITY, OR TORT (INCLUDING NEGLIGENCE OR OTHERWISE)
+// ARISING IN ANY WAY OUT OF THE USE OF THIS SOFTWARE, EVEN IF ADVISED
+// OF THE POSSIBILITY OF SUCH DAMAGE.
+
+package net.grinder.engine.process;
+
+import java.util.ArrayList;
+import java.util.List;
+import java.util.Random;
+import java.util.concurrent.ExecutorService;
+import java.util.concurrent.Executors;
+import java.util.concurrent.RejectedExecutionException;
+import java.util.concurrent.TimeUnit;
+import java.util.concurrent.atomic.AtomicInteger;
+
+import junit.framework.TestCase;
+import net.grinder.common.UncheckedGrinderException;
+import net.grinder.engine.common.EngineException;
+import net.grinder.engine.process.ScriptEngine.TestInstrumentation;
+import net.grinder.testutility.RandomStubFactory;
+
+
+/**
+ * Unit tests for {@link InstrumentationLocator}.
+ *
+ * @author Philip Aston
+ * @version $Revision:$
+ */
+public class TestInstrumentationLocator extends TestCase {
+
+  private final RandomStubFactory<TestInstrumentation>
+    m_instrumentationStubFactory =
+      RandomStubFactory.create(TestInstrumentation.class);
+  private final TestInstrumentation m_instrumentation =
+    m_instrumentationStubFactory.getStub();
+
+  private final RandomStubFactory<TestInstrumentation>
+    m_instrumentationStubFactory2 =
+      RandomStubFactory.create(TestInstrumentation.class);
+  private final TestInstrumentation m_instrumentation2 =
+    m_instrumentationStubFactory2.getStub();
+
+  @Override protected void tearDown() throws Exception {
+    super.tearDown();
+    InstrumentationLocator.getInstrumentation().clear();
+  }
+
+  public void testNullBehaviour() throws Exception {
+    InstrumentationLocator.enter(this, "foo");
+    InstrumentationLocator.exit(this, "foo", false);
+
+    assertEquals(0, InstrumentationLocator.getInstrumentation().size());
+  }
+
+  public void testSingleRegistration() throws Exception {
+    final Object target = new Object();
+
+    InstrumentationLocator.register(target, "location", m_instrumentation);
+    assertEquals(1, InstrumentationLocator.getInstrumentation().size());
+    m_instrumentationStubFactory.assertNoMoreCalls();
+
+    InstrumentationLocator.enter(target, "location");
+    m_instrumentationStubFactory.assertSuccess("startTest");
+    m_instrumentationStubFactory.assertNoMoreCalls();
+
+    InstrumentationLocator.exit(target, "location", true);
+    m_instrumentationStubFactory.assertSuccess("endTest", true);
+    m_instrumentationStubFactory.assertNoMoreCalls();
+
+    InstrumentationLocator.enter(this, "location");
+    InstrumentationLocator.exit(this, "location", true);
+    m_instrumentationStubFactory.assertNoMoreCalls();
+
+    InstrumentationLocator.enter(target, "location2");
+    InstrumentationLocator.exit(target, "location2", true);
+    m_instrumentationStubFactory.assertNoMoreCalls();
+
+    InstrumentationLocator.enter(target, "location");
+    m_instrumentationStubFactory.assertSuccess("startTest");
+    m_instrumentationStubFactory.assertNoMoreCalls();
+
+    InstrumentationLocator.exit(target, "location", false);
+    m_instrumentationStubFactory.assertSuccess("endTest", false);
+    m_instrumentationStubFactory.assertNoMoreCalls();
+
+    // Interned strings shouldn't match.
+    InstrumentationLocator.enter(target, new String("location"));
+    InstrumentationLocator.exit(target, new String("location"), true);
+    m_instrumentationStubFactory.assertNoMoreCalls();
+  }
+
+  public void testBadRegistration() throws Exception {
+    final Object target = new Object();
+
+    final EngineException exception = new EngineException("bork");
+
+    m_instrumentationStubFactory.setThrows("startTest", exception);
+    m_instrumentationStubFactory.setThrows("endTest", exception);
+
+    InstrumentationLocator.register(target, "location", m_instrumentation);
+    assertEquals(1, InstrumentationLocator.getInstrumentation().size());
+    m_instrumentationStubFactory.assertNoMoreCalls();
+
+    try {
+      InstrumentationLocator.enter(target, "location");
+      fail("Expected UncheckedGrinderException");
+    }
+    catch (UncheckedGrinderException e) {
+      assertSame(exception, e.getCause());
+    }
+    m_instrumentationStubFactory.assertException("startTest", exception);
+    m_instrumentationStubFactory.assertNoMoreCalls();
+
+    try {
+      InstrumentationLocator.exit(target, "location", false);
+      fail("Expected UncheckedGrinderException");
+    }
+    catch (UncheckedGrinderException e) {
+      assertSame(exception, e.getCause());
+    }
+    m_instrumentationStubFactory.assertException("endTest", exception, false);
+    m_instrumentationStubFactory.assertNoMoreCalls();
+  }
+
+  public void testStaticRegistration() throws Exception {
+    InstrumentationLocator.register(null, "location", m_instrumentation);
+    assertEquals(1, InstrumentationLocator.getInstrumentation().size());
+    m_instrumentationStubFactory.assertNoMoreCalls();
+
+    InstrumentationLocator.enter(null, "location");
+    m_instrumentationStubFactory.assertSuccess("startTest");
+    m_instrumentationStubFactory.assertNoMoreCalls();
+
+    InstrumentationLocator.exit(null, "location", true);
+    m_instrumentationStubFactory.assertSuccess("endTest", true);
+    m_instrumentationStubFactory.assertNoMoreCalls();
+
+    InstrumentationLocator.enter(this, "location");
+    InstrumentationLocator.exit(this, "location", true);
+    m_instrumentationStubFactory.assertNoMoreCalls();
+
+    InstrumentationLocator.enter(null, "location2");
+    InstrumentationLocator.exit(null, "location2", true);
+    m_instrumentationStubFactory.assertNoMoreCalls();
+  }
+
+  public void testMultipleRegistrations() throws Exception {
+    final Object target = new Object();
+    final Object target2 = new Object();
+
+    InstrumentationLocator.register(target, "location", m_instrumentation);
+    assertEquals(1, InstrumentationLocator.getInstrumentation().size());
+
+    InstrumentationLocator.enter(target2, "location");
+    InstrumentationLocator.exit(target2, "location", false);
+    m_instrumentationStubFactory.assertNoMoreCalls();
+
+    InstrumentationLocator.enter(target, "location");
+    m_instrumentationStubFactory.assertSuccess("startTest");
+    m_instrumentationStubFactory.assertNoMoreCalls();
+
+    InstrumentationLocator.exit(target, "location", true);
+    m_instrumentationStubFactory.assertSuccess("endTest", true);
+    m_instrumentationStubFactory.assertNoMoreCalls();
+
+    InstrumentationLocator.register(target2, "location", m_instrumentation2);
+    assertEquals(1, InstrumentationLocator.getInstrumentation().size());
+
+    InstrumentationLocator.register(target2, "location2", m_instrumentation);
+    assertEquals(2, InstrumentationLocator.getInstrumentation().size());
+
+    InstrumentationLocator.enter(target, "location");
+    m_instrumentationStubFactory.assertSuccess("startTest");
+    m_instrumentationStubFactory.assertNoMoreCalls();
+
+    InstrumentationLocator.enter(target2, "location");
+    m_instrumentationStubFactory2.assertSuccess("startTest");
+    m_instrumentationStubFactory2.assertNoMoreCalls();
+
+    InstrumentationLocator.exit(target, "location", true);
+    m_instrumentationStubFactory.assertSuccess("endTest", true);
+    m_instrumentationStubFactory.assertNoMoreCalls();
+
+    InstrumentationLocator.exit(target2, "location", false);
+    m_instrumentationStubFactory2.assertSuccess("endTest", false);
+    m_instrumentationStubFactory2.assertNoMoreCalls();
+    m_instrumentationStubFactory.assertNoMoreCalls();
+
+    InstrumentationLocator.enter(target2, "location2");
+    m_instrumentationStubFactory.assertSuccess("startTest");
+    m_instrumentationStubFactory.assertNoMoreCalls();
+
+    InstrumentationLocator.exit(target2, "location2", true);
+    m_instrumentationStubFactory.assertSuccess("endTest", true);
+    m_instrumentationStubFactory.assertNoMoreCalls();
+  }
+
+  public void testNestedRegistrations() throws Exception {
+    final Object target = new Object();
+
+    InstrumentationLocator.register(target, "location", m_instrumentation);
+    InstrumentationLocator.register(target, "location", m_instrumentation);
+    InstrumentationLocator.register(target, "location", m_instrumentation2);
+    assertEquals(1, InstrumentationLocator.getInstrumentation().size());
+
+    InstrumentationLocator.enter(target, "location");
+    m_instrumentationStubFactory.assertSuccess("startTest");
+    m_instrumentationStubFactory.assertSuccess("startTest");
+    m_instrumentationStubFactory2.assertSuccess("startTest");
+    m_instrumentationStubFactory.assertNoMoreCalls();
+    m_instrumentationStubFactory2.assertNoMoreCalls();
+
+    InstrumentationLocator.exit(target, "location", false);
+    m_instrumentationStubFactory2.assertSuccess("endTest", false);
+    m_instrumentationStubFactory.assertSuccess("endTest", false);
+    m_instrumentationStubFactory.assertSuccess("endTest", false);
+    m_instrumentationStubFactory.assertNoMoreCalls();
+    m_instrumentationStubFactory2.assertNoMoreCalls();
+  }
+
+  public void testConcurrency() throws Exception {
+    final ExecutorService executor = Executors.newCachedThreadPool();
+
+    final AtomicInteger runs = new AtomicInteger(0);
+    final AtomicInteger n = new AtomicInteger(0);
+
+    final TestInstrumentation instrumentation = new TestInstrumentation() {
+      public void startTest() throws EngineException {
+        n.incrementAndGet();
+      }
+
+      public void endTest(boolean success) throws EngineException {
+        n.decrementAndGet();
+      }
+    };
+
+    final Random random = new Random();
+
+    final String[] locations = { "L1", "L2", "L3" };
+
+    class RegisterInstrumentation implements Runnable {
+      public void run() {
+        runs.incrementAndGet();
+
+        final String location = locations[random.nextInt(locations.length)];
+
+        if (random.nextInt(10) == 0) {
+          InstrumentationLocator.register(this, location, instrumentation);
+        }
+
+        InstrumentationLocator.enter(this, location);
+        InstrumentationLocator.exit(this, location, true);
+
+        try {
+          executor.execute(this);
+        }
+        catch (RejectedExecutionException e) {
+        }
+      }}
+
+    final List<Runnable> runnables = new ArrayList<Runnable>();
+
+    for (int i = 0; i < 10; ++i) {
+      runnables.add(new RegisterInstrumentation());
+    }
+
+    for (Runnable r : runnables) {
+      executor.execute(r);
+    }
+
+    while (runs.get() < 10000) {
+      Thread.sleep(10);
+    }
+    executor.shutdown();
+    assertTrue(executor.awaitTermination(1, TimeUnit.SECONDS));
+
+    assertEquals(0, n.get());
+  }
+}
