5c53ebac2b67315d5c78ea3e539b9a7be0e7bd8c
Ryan Gardner <ryan.gardner@dealer.com>
2012-06-28 08:01:16 -0400

    Move the test creation and recording to the static block for performance reasons

--- grinder/examples/helloworld.groovy
+++ grinder/examples/helloworld.groovy
@@ -1,11 +1,15 @@
 import net.grinder.script.Grinder;
-import net.grinder.script.Test;
+import net.grinder.script.Test
+import groovy.transform.CompileStatic
 
+// the class name is arbitrary
 class hiWorld {
-    Test test1 = new Test(1, "hello world");
+    Test test1 = new Test(1, "hello world")
+    test1.record (Grinder.grinder.logger)
 
+    // the closure named "testRunner" is what will be called
+    // for each test
     def testRunner = {
-        test1.record(Grinder.grinder.logger)
         Grinder.grinder.logger.info("hello world")
     }
 }
