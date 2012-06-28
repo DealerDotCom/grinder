f18d1aff4aecf35ad5872d28f1521f5e4393bbff
Ryan Gardner <ryan.gardner@dealer.com>
2012-06-27 22:50:06 -0400

    update the helloworld.groovy to a functioning version

--- grinder/examples/helloworld.groovy
+++ grinder/examples/helloworld.groovy
@@ -1,5 +1,11 @@
-class g {
+import net.grinder.script.Grinder;
+import net.grinder.script.Test;
+
+class hiWorld {
+    Test test1 = new Test(1, "hello world");
+
     def testRunner = {
-        println "hello world"
+        test1.record(Grinder.grinder.logger)
+        Grinder.grinder.logger.info("hello world")
     }
 }
